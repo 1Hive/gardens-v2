00d105dcbfc0f5dffe9839d7f5e293139f211450
Felipe Novaes F Rocha <windholyghost@gmail.com>
2023-12-11 23:32:30 -0300

    :sparkles: CV Mock and Allo test

---
+++ pkg/contracts/test/AlloTest.t.sol
@@ -0,0 +1,84 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+import "forge-std/Test.sol";
+
+import {IAllo} from "allo-v2/core/interfaces/IAllo.sol";
+import {IStrategy} from "allo-v2/core/interfaces/IStrategy.sol";
+
+import {Allo} from "allo-v2/core/Allo.sol";
+import {Registry} from "allo-v2/core/Registry.sol";
+
+import {Native} from "allo-v2/core/libraries/Native.sol";
+import {Errors} from "allo-v2/core/libraries/Errors.sol";
+import {Metadata} from "allo-v2/core/libraries/Metadata.sol";
+
+import {AlloSetup} from "allo-v2/../test/foundry/shared/AlloSetup.sol";
+import {RegistrySetupFull} from "allo-v2/../test/foundry/shared/RegistrySetup.sol";
+
+import {GasHelpers} from "allo-v2/../test/utils/GasHelpers.sol";
+import {MockERC20} from "allo-v2/../test/utils/MockERC20.sol";
+
+import {CVMockStrategy} from "./CVMockStrategy.sol";
+
+contract TestAllo is Test, AlloSetup, RegistrySetupFull, Native, Errors, GasHelpers {
+
+    address public strategy;
+    MockERC20 public token;
+    uint256 public mintAmount = 1_000_000 * 10 ** 18;
+
+    Metadata public metadata = Metadata({protocol: 1, pointer: "strategy pointer"});
+
+    function setUp() public {
+        __RegistrySetupFull();
+        __AlloSetup(address(registry()));
+
+        vm.startPrank(allo_owner());
+        allo().updateBaseFee(0);
+        allo().updatePercentFee(0);
+        vm.stopPrank();
+
+        token = new MockERC20();
+        token.mint(local(), mintAmount);
+        token.mint(allo_owner(), mintAmount);
+        token.mint(pool_admin(), mintAmount);
+        token.approve(address(allo()), mintAmount);
+
+        vm.prank(pool_admin());
+        token.approve(address(allo()), mintAmount);
+
+        strategy = address(new CVMockStrategy(address(allo())));
+
+        vm.startPrank(allo_owner());
+        allo().transferOwnership(local());
+        vm.stopPrank();
+    }
+
+    event PoolCreated(
+    uint256 indexed poolId,
+    bytes32 indexed profileId,
+    IStrategy strategy,
+    address token,
+    uint256 amount,
+    Metadata metadata
+);
+
+    function test_createPool() public {
+        startMeasuringGas("createPool");
+        allo().addToCloneableStrategies(strategy);
+
+        vm.expectEmit(true, true, false, false);
+        emit PoolCreated(1, poolProfile_id(), IStrategy(strategy), NATIVE, 0, metadata);
+
+        vm.prank(pool_admin());
+
+        uint256 poolId = allo().createPool(poolProfile_id(), strategy, "0x", NATIVE, 0, metadata, pool_managers());
+//
+//        IAllo.Pool memory pool = allo().getPool(poolId);
+//        stopMeasuringGas();
+//
+//        assertEq(pool.profileId, poolProfile_id());
+//        assertNotEq(address(pool.strategy), address(strategy));
+//
+    }
+}
---
+++ pkg/contracts/test/CVMockStrategy.sol
@@ -0,0 +1,92 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity ^0.8.19;
+
+import "allo-v2/strategies/BaseStrategy.sol";
+
+contract CVMockStrategy is BaseStrategy {
+    uint256 internal surpressStateMutabilityWarning;
+
+    constructor(address _allo) BaseStrategy(_allo, "MockStrategy") {}
+
+    function initialize(uint256 _poolId, bytes memory _data) external {
+        __BaseStrategy_init(_poolId);
+        _data;
+        emit Initialized(_poolId, _data);
+    }
+
+    // this is called via allo.sol to register recipients
+    // it can change their status all the way to Accepted, or to Pending if there are more steps
+    // if there are more steps, additional functions should be added to allow the owner to check
+    // this could also check attestations directly and then Accept
+    function _registerRecipient(bytes memory _data, address _sender) internal override returns (address) {
+        surpressStateMutabilityWarning++;
+        _data;
+        return _sender;
+    }
+
+    // only called via allo.sol by users to allocate to a recipient
+    // this will update some data in this contract to store votes, etc.
+    function _allocate(bytes memory _data, address _sender) internal override {
+        surpressStateMutabilityWarning++;
+        _data;
+        _sender;
+    }
+
+    // this will distribute tokens to recipients
+    // most strategies will track a TOTAL amount per recipient, and a PAID amount, and pay the difference
+    // this contract will need to track the amount paid already, so that it doesn't double pay
+    function _distribute(address[] memory _recipientIds, bytes memory _data, address _sender) internal override {
+        surpressStateMutabilityWarning++;
+        _recipientIds;
+        _data;
+        _sender;
+    }
+
+    // simply returns the status of a recipient
+    // probably tracked in a mapping, but will depend on the implementation
+    // for example, the OpenSelfRegistration only maps users to bool, and then assumes Accepted for those
+    // since there is no need for Pending or Rejected
+    function _getRecipientStatus(address _recipientId) internal view override returns (Status) {
+        surpressStateMutabilityWarning;
+        return _recipientId == address(0) ? Status.Rejected : Status.Accepted;
+    }
+
+    /// @return Input the values you would send to distribute(), get the amounts each recipient in the array would receive
+    function getPayouts(address[] memory _recipientIds, bytes[] memory _data)
+        external
+        view
+        override
+        returns (PayoutSummary[] memory)
+    {
+        surpressStateMutabilityWarning;
+
+        PayoutSummary[] memory payouts = new PayoutSummary[](_recipientIds.length);
+
+        for (uint256 i; i < _recipientIds.length; i++) {
+            payouts[i] = abi.decode(_data[i], (PayoutSummary));
+        }
+
+        return payouts;
+    }
+
+    function _getPayout(address _recipientId, bytes memory _data)
+        internal
+        view
+        override
+        returns (PayoutSummary memory)
+    {
+        surpressStateMutabilityWarning;
+        _data;
+        return PayoutSummary(_recipientId, 0);
+    }
+
+    // simply returns whether a allocator is valid or not, will usually be true for all
+    function _isValidAllocator(address _allocator) internal view override returns (bool) {
+        surpressStateMutabilityWarning;
+        return _allocator == address(0) ? false : true;
+    }
+
+    function setPoolActive(bool _active) external {
+        _setPoolActive(_active);
+    }
+}
