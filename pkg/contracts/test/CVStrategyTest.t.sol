bff1f875b09da83639eae7c3fbcb58de5eab61ef
Felipe Novaes F Rocha <windholyghost@gmail.com>
2023-12-26 23:51:02 -0300

    :fire: remove pinned solc version in foundry :bug: add missing _pow func to avoid overflow and underflow :sparkles: helper func in test :test_tube: conviction value check after vote and time passed

--- pkg/contracts/test/CVStrategyTest.t.sol
+++ pkg/contracts/test/CVStrategyTest.t.sol
@@ -40,6 +40,10 @@ contract CVStrategyTest is Test, AlloSetup, RegistrySetupFull, Native, Errors, G
     Metadata public metadata = Metadata({protocol: 1, pointer: "strategy pointer"});
 
     RegistryGardens internal registryGardens;
+    uint256 internal constant TWO_127 = 2 ** 127;
+    uint256 internal constant TWO_128 = 2 ** 128;
+    uint256 internal constant D = 10 ** 7;
+
 
     function setUp() public {
         __RegistrySetupFull();
@@ -122,9 +126,9 @@ contract CVStrategyTest is Test, AlloSetup, RegistrySetupFull, Native, Errors, G
         vm.startPrank(pool_admin());
 
         CVStrategy.InitializeParams memory params;
-        params.decay = 0.9 ether / 10 ** 11; // alpha = decay
-        params.maxRatio = 0.2 ether / 10 ** 11; // beta = maxRatio
-        params.weight = 0.002 ether / 10 ** 11; // RHO = p  = weight
+        params.decay = _etherToFloat(0.9 ether); // alpha = decay
+        params.maxRatio = _etherToFloat(0.2 ether); // beta = maxRatio
+        params.weight = _etherToFloat(0.002 ether); // RHO = p  = weight
         params.minThresholdStakePercentage = 0.2 ether; // 20%
         params.registryGardens = address(_registryGardens());
 
@@ -219,6 +223,40 @@ contract CVStrategyTest is Test, AlloSetup, RegistrySetupFull, Native, Errors, G
     //     // console.log("Submitter: %s", submitter);
     // }
 
+    function _etherToFloat(uint256 _amount) internal pure returns (uint256) {
+        return _amount / 10 ** 11;
+    }
+
+    function _mul(uint256 _a, uint256 _b) internal pure returns (uint256 _result) {
+        require(_a <= TWO_128, "_a should be less than or equal to 2^128");
+        require(_b < TWO_128, "_b should be less than 2^128");
+        return ((_a * _b) + TWO_127) >> 128;
+    }
+
+    function _pow(uint256 _a, uint256 _b) internal pure returns (uint256 _result) {
+        require(_a < TWO_128, "_a should be less than 2^128");
+        uint256 a = _a;
+        uint256 b = _b;
+        _result = TWO_128;
+        while (b > 0) {
+            if (b & 1 == 0) {
+                a = _mul(a, a);
+                b >>= 1;
+            } else {
+                _result = _mul(_result, a);
+                b -= 1;
+            }
+        }
+    }
+    function _calculateConviction(uint256 _timePassed, uint256 _lastConv, uint256 _oldAmount, uint256 decay)
+        public
+        pure
+        returns (uint256)
+    {
+        uint256 t = _timePassed;
+        uint256 atTWO_128 = _pow((decay << 128) / D,t);
+        return (((atTWO_128 * _lastConv) + (_oldAmount * D * (TWO_128 - atTWO_128) / (D - decay))) + TWO_127) >> 128;
+    }
     function test_proposalSupported_2_times() public {
         (IAllo.Pool memory pool, uint256 poolId) = _createProposal();
 
@@ -250,7 +288,32 @@ contract CVStrategyTest is Test, AlloSetup, RegistrySetupFull, Native, Errors, G
 
         assertEq(cv.getProposalVoterStake(1, address(pool_admin())), 50); // 100% of 50 = 50
         assertEq(cv.getProposalStakedAmount(1), 40 + 50);
+    }
 
+    function test_proposalSupported_conviction_check() public {
+        (IAllo.Pool memory pool, uint256 poolId) = _createProposal();
+
+        /**
+         * ASSERTS
+         */
+        startMeasuringGas("Support a Proposal");
+        CVStrategy.ProposalSupport[] memory votes = new CVStrategy.ProposalSupport[](1);
+        votes[0] = CVStrategy.ProposalSupport(1, 80); // 0 + 70 = 70% = 35
+        bytes memory data = abi.encode(votes);
+        allo().allocate(poolId, data);
+        stopMeasuringGas();
+
+        uint256 AMOUNT_STAKED = 40;
+        CVStrategy cv = CVStrategy(payable(address(pool.strategy)));
+        assertEq(cv.getProposalVoterStake(1, address(this)), AMOUNT_STAKED); // 80% of 50 = 40
+        assertEq(cv.getProposalStakedAmount(1), AMOUNT_STAKED); // 80% of 50 = 40
+
+        uint256 cv_amount = cv.calculateConviction(10, 0, AMOUNT_STAKED);
+        console.log("cv_amount: %s", cv_amount);
+        uint256 cv_cmp = _calculateConviction(10, 0, AMOUNT_STAKED, 0.9 ether / 10 ** 11);
+        console.log("cv_cmp: %s", cv_cmp);
+        assertEq(cv_amount, cv_cmp);
+     
     }
 
     function testRevert_allocate_removeSupport_wo_support_before_SUPPORT_UNDERFLOW() public {
@@ -272,8 +335,9 @@ contract CVStrategyTest is Test, AlloSetup, RegistrySetupFull, Native, Errors, G
         CVStrategy cv = CVStrategy(payable(address(pool.strategy)));
 
         assertEq(cv.getProposalVoterStake(1, address(this)), 0, "VoterStakeAmount"); // 100% of 50 = 50
-        assertEq(cv.getProposalStakedAmount(1), 0,"TotalStakedAmountInProposal");
+        assertEq(cv.getProposalStakedAmount(1), 0, "TotalStakedAmountInProposal");
     }
+
     function test_allocate_proposalSupport_empty_array() public {
         (IAllo.Pool memory pool, uint256 poolId) = _createProposal();
 
