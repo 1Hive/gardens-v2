b0111913599d2dc21943844275142dd9db670efc
Felipe Novaes F Rocha <windholyghost@gmail.com>
2024-01-26 01:39:14 -0300

    :bug: fix encodeFunctionParam abi array :memo: update address

--- apps/web/constants/contracts/index.ts
+++ apps/web/constants/contracts/index.ts
@@ -28,7 +28,7 @@ const envRpcUrlArbTestnet = process.env.NEXT_PUBLIC_RPC_URL_ARB_TESTNET || "";
 
 let __contractsAddresses = {
   allo: `0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0` as `0x${string}`,
-  strategy: `0xB7f8BC63BbcaD18155201308C8f3540b07f84F5e` as `0x${string}`,
+  strategy: `0xA51c1fc2f0D1a1b8494Ed1FE312d7C3a78Ed91C0` as `0x${string}`,
   registryGardens:
     `0x61c36a8d610163660E21a8b7359e1Cac0C9133e1` as `0x${string}`,
   registry: `0x5FbDB2315678afecb367f032d93F642f64180aa3` as `0x${string}`,
