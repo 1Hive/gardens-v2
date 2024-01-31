c0d9b995231163d14563e7aa5e77546cba7c5fc2
Felipe Novaes F Rocha <windholyghost@gmail.com>
2024-01-31 12:54:30 -0300

    :memo: arweave cid

--- pkg/subgraph/src/mappings/proposalMetadata.ts
+++ pkg/subgraph/src/mappings/proposalMetadata.ts
@@ -1,6 +1,6 @@
 import { Bytes, dataSource, log, json, BigInt } from "@graphprotocol/graph-ts";
 import { ProposalMetadata } from "../../generated/schema";
-
+// https://kdghxz4drlsffkptavafm6ws2qkdwpdqauxstqioi3xdli7yjo4q.arweave.net/UMx754OK5FKp8wVAVnrS1BQ7PHAFLynBDkbuNaP4S7k
 export function handleProposalMetadata(content: Bytes): void {
   let hash = dataSource.stringParam();
   log.debug("handleProposalMetadata1", [hash]);
