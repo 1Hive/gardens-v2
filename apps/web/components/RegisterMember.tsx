e42ccf6bac2109e4aadd7596a9a7a2725194db01
Luciano Scaminaci <scaminaci.luciano@gmail.com>
2024-02-06 13:15:26 -0300

    refactor: RegisterMember

--- apps/web/components/RegisterMember.tsx
+++ apps/web/components/RegisterMember.tsx
@@ -1,65 +1,84 @@
 "use client";
-import React, { useEffect } from "react";
-import { useContractRead, useContractWrite, useAccount } from "wagmi";
+import React, { useEffect, useState } from "react";
+import {
+  useContractReads,
+  useContractWrite,
+  useAccount,
+  useChainId,
+} from "wagmi";
 import { Button } from "./Button";
 import { toast } from "react-toastify";
 import useErrorDetails from "@/utils/getErrorName";
 import {
   confirmationsRequired,
-  contractsAddresses,
+  getContractsAddrByChain,
 } from "@/constants/contracts";
 import { useViemClient } from "@/hooks/useViemClient";
 import { erc20ABI, registryCommunityABI } from "@/src/generated";
 import { abiWithErrors } from "@/utils/abiWithErrors";
+import { getBuiltGraphSDK } from "#/subgraph/.graphclient";
 
 export function RegisterMember() {
   const { address } = useAccount();
   const viemClient = useViemClient();
+  const chainId = useChainId();
+  const contractsAddresses = getContractsAddrByChain(chainId);
+  const [isMemberRegistered, setIsMemberRegistered] = useState<
+    boolean | undefined
+  >(undefined);
+  const [registerStakeAmount, setRegisterStakeAmount] = useState<number>(0);
+  const [gardenToken, setGardenToken] = useState<`0x${string}` | undefined>(
+    undefined,
+  );
+
+  const registryContractCallConfig = {
+    address: contractsAddresses?.registryCommunity,
+    abi: abiWithErrors(registryCommunityABI),
+  };
 
   const {
-    data: isMemberRegistered,
-    error: errorMemberRegistered,
-    status: isMemberStatus,
-  } = useContractRead({
-    address: contractsAddresses.registryCommunity,
-    abi: abiWithErrors(registryCommunityABI),
-    functionName: "isMember",
-    args: [address || "0x"],
-    watch: true,
+    data: contractsData,
+    error,
+    isSuccess,
+  } = useContractReads({
+    contracts: [
+      {
+        ...registryContractCallConfig,
+        functionName: "isMember",
+        args: [address || "0x"],
+      },
+      {
+        ...registryContractCallConfig,
+        functionName: "registerStakeAmount",
+      },
+      {
+        ...registryContractCallConfig,
+        functionName: "gardenToken",
+      },
+    ],
   });
 
-  console.log(contractsAddresses.registryCommunity);
-  
-  const {
-    data: gardenTokenAddress,
-    error: errorGardenToken,
-    status: gardenTokenStatus,
-  } = useContractRead({
-    address: contractsAddresses.registryCommunity,
-    abi: abiWithErrors(registryCommunityABI),
-    functionName: "gardenToken",
-    watch: true,
-  });
+  useEffect(() => {
+    if (isSuccess && contractsData !== undefined) {
+      if (contractsData[0]?.status === "success") {
+        setIsMemberRegistered(contractsData[0]?.result as boolean);
+      }
+      if (contractsData[1]?.status === "success") {
+        setRegisterStakeAmount(Number(contractsData[1]?.result as BigInt));
+      }
+      if (contractsData[2]?.status === "success") {
+        setGardenToken(contractsData[2]?.result as `0x${string}`);
+      }
+    }
+  }, [contractsData]);
 
-  const {
-    data: amountData,
-    error: errorAmount,
-    status: amountStatus,
-  } = useContractRead({
-    address: contractsAddresses.registryCommunity,
-    abi: abiWithErrors(registryCommunityABI),
-    functionName: "registerStakeAmount",
-    watch: true,
-  });
-console.log(amountData)
   const {
     data: registerMemberData,
     write: writeRegisterMember,
     error: errorRegisterMember,
     isSuccess: isSuccessRegisterMember,
   } = useContractWrite({
-    address: contractsAddresses.registryCommunity,
-    abi: abiWithErrors(registryCommunityABI),
+    ...registryContractCallConfig,
     functionName: "stakeAndRegisterMember",
   });
 
@@ -69,8 +88,7 @@ console.log(amountData)
     error: errorUnregisterMember,
     isSuccess: isSuccessUnregisterMember,
   } = useContractWrite({
-    address: contractsAddresses.registryCommunity,
-    abi: abiWithErrors(registryCommunityABI),
+    ...registryContractCallConfig,
     functionName: "unregisterMember",
   });
 
@@ -80,17 +98,17 @@ console.log(amountData)
     error: errorAllowToken,
     isSuccess: isSuccessAllowToken,
   } = useContractWrite({
-    address: gardenTokenAddress as `0x${string}`,
+    address: gardenToken as `0x${string}`,
     abi: abiWithErrors(erc20ABI),
-    args: [contractsAddresses.registryCommunity, amountData], // allowed spender address, amount
+    args: [contractsAddresses?.registryCommunity, BigInt(registerStakeAmount)], // allowed spender address, amount
     functionName: "approve",
   });
 
   useErrorDetails(errorRegisterMember, "stakeAndRegisterMember");
   useErrorDetails(errorUnregisterMember, "unregisterMember");
-  useErrorDetails(errorMemberRegistered, "isMember");
-  useErrorDetails(errorAmount, "approve");
-  useErrorDetails(errorGardenToken, "gardenToken");
+  // useErrorDetails(errorMemberRegistered, "isMember");
+  // useErrorDetails(errorAmount, "approve");
+  // useErrorDetails(errorGardenToken, "gardenToken");
 
   const transactionReceipt = async () =>
     await viemClient.waitForTransactionReceipt({
