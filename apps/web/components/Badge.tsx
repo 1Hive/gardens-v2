681a57de0d227083b21895ad63b514460a9be4c2
Luciano Scaminaci <scaminaci.luciano@gmail.com>
2024-02-17 14:16:42 -0300

    refactor: Badge & StatusBadge

--- apps/web/components/Badge.tsx
+++ apps/web/components/Badge.tsx
@@ -1,53 +1,37 @@
 import React from "react";
-import type { HTMLAttributes } from "react";
-import cn from "classnames";
-
-export type Type = "signaling" | "funding" | "streaming";
-type Status =
-  | "1"
-  | "paused"
-  | "cancelled"
-  | "executed"
-  | "inactive"
-  | string;
-
-type TypeStyles = Record<Type, HTMLAttributes<HTMLSpanElement>["className"]>;
-type StatusStyles = Record<
-  Status,
-  HTMLAttributes<HTMLSpanElement>["className"]
->;
 
 interface BadgeProps {
-  type: Type;
+  type: number;
   classNames?: string;
 }
 
 interface StatusBadgeProps {
-  status: Status;
+  status: number;
   classNames?: string;
 }
 
-// TODO: add real styles, this is just a placeholder
+// TODO!: add real styles, this is just a placeholder
 //variant for common badge
-const TYPE_STYLES: TypeStyles = {
-  funding: "bg-primary text-black",
-  streaming: "bg-primary text-white",
-  signaling: "bg-secondary text-white",
-};
+const TYPE_STYLES = [
+  "bg-primary text-black",
+  "bg-primary text-white",
+  "bg-secondary text-white",
+];
 
 //variants for Statys Badge
-const STATUS_STYLES: StatusStyles = {
-  1: "badge-success",
-  paused: "bg-primary",
-  cancelled: "bg-warning",
-  executed: "bg-primary",
-  inactive: "bg-error",
-};
+const STATUS_STYLES = [
+  "badge-success",
+  "bg-primary",
+  "bg-warning",
+  "bg-primary",
+  "bg-error",
+];
 
 const BASE_STYLES = "badge w-28 p-4 font-semibold";
 const BASE_STYLES_STATUS = "badge text-white min-w-20 p-4 text-center";
 
 export function Badge({ type, classNames }: BadgeProps) {
+  type = type - 1;
   return (
     <>
       <span
@@ -55,21 +39,32 @@ export function Badge({ type, classNames }: BadgeProps) {
           TYPE_STYLES[type] ?? "bg-accent text-black"
         } ${BASE_STYLES} ${classNames}`}
       >
-        {type ?? "no type"}
+        {getTypeName(Number(type)) ?? "no type"}
       </span>
     </>
   );
 }
 
 export function StatusBadge({ status, classNames }: StatusBadgeProps) {
+  status = status - 1;
   return (
     <>
       <span
         className={`${STATUS_STYLES[status] ?? "bg-primary"} 
            ${BASE_STYLES_STATUS} ${classNames}`}
       >
-        {status ?? "no type"}
+        {getStatusName(status) ?? "no status"}
       </span>
     </>
   );
 }
+
+const getTypeName = (type: number) => {
+  const typeArray = ["funding", "signaling", "streaming"];
+  return typeArray[type];
+};
+
+const getStatusName = (status: number) => {
+  const statusArray = ["active", "paused", "cancelled", "executed", "inactive"];
+  return statusArray[status];
+};
