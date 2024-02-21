256bda76ff37d432979deac99dfd873d4b1ed758
Matias <mantitaaa89@gmail.com>
2024-02-21 14:15:51 -0300

    feat: add new header styles and  layout at `GardenPage`with new images

--- apps/web/assets/index.ts
+++ apps/web/assets/index.ts
@@ -6,6 +6,11 @@ import honeyIcon_ from "./honeyIcon.svg";
 import gardenFlower_ from "./garden-flower.svg";
 import flowers_ from "./yellow-blue-flowers.svg";
 import newLogo_ from "./NewLogo.png";
+import tree1_ from "./TreeOne.svg";
+import tree2_ from "./TreeTwo.svg";
+import tree3_ from "./TreeThree.svg";
+import grass_ from "./Grass.svg";
+import grassLarge_ from "./GrassLarge.svg";
 
 export const clouds1 = clouds1_;
 export const clouds2 = clouds2_;
@@ -15,5 +20,10 @@ export const honeyIcon = honeyIcon_;
 export const gardenFlower = gardenFlower_;
 export const flowers = flowers_;
 export const newLogo = newLogo_;
+export const tree1 = tree1_;
+export const tree2 = tree2_;
+export const tree3 = tree3_;
+export const grass = grass_;
+export const grassLarge = grassLarge_;
 
 export { GardensLogo } from "./GardensLogo";
