ef6938f7f07f54cf3f51a775541a8199170ea661
Matias <mantitaaa89@gmail.com>
2024-02-21 20:06:32 -0300

    styles: renewed style at `PoolCard` + minor fix `CommunityCard`

--- apps/web/components/CommunityCard.tsx
+++ apps/web/components/CommunityCard.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Button, RegisterMember } from "@/components";
 import {
   ChevronDownIcon,
@@ -10,8 +10,6 @@ import { PoolCard } from "@/components";
 import { Address, useAccount } from "wagmi";
 import { getCommunityByGardenQuery } from "#/subgraph/.graphclient";
 import { formatAddress } from "@/utils/formatAddress";
-
-import Image from "next/image";
 
 type CommunityQuery = NonNullable<
   NonNullable<getCommunityByGardenQuery["tokenGarden"]>["communities"]
@@ -85,9 +83,9 @@ export function CommunityCard({
               <div className="stat-desc">2100k in total funds</div>
             </div>
           </div>
-          <div className="border2 card-actions justify-end">
+          <div className="card-actions mt-10 justify-end">
             <div
-              className={`flex w-full transform flex-wrap gap-4 overflow-hidden border-2 border-violet-500 p-4 transition-height duration-200 ease-in-out ${
+              className={`flex w-full transform flex-wrap gap-4 overflow-hidden transition-height duration-200 ease-in-out ${
                 !open && "max-h-[290px]"
               } `}
             >
@@ -110,45 +108,6 @@ export function CommunityCard({
           </div>
         </main>
       </div>
-      {/* <div className="flex flex-col items-center justify-center gap-8 rounded-xl border-2 border-black bg-info p-8 transition-all duration-200 ease-in-out">
-        <div className="relative flex w-full items-center justify-center">
-          <p className="absolute left-0 top-[50%] m-0 translate-y-[-50%] font-bold">
-            Community Pools: {pools.length}
-          </p>
-          <h3 className="m-0 font-press text-lg text-info-content">{name}</h3>
-          <p className="absolute right-0 top-[50%] m-0 translate-y-[-50%] font-bold">
-            {formatAddress(communityAddress)}
-          </p>
-        </div>
-        <RegisterMember
-          // isMember={isMember}
-          communityAddress={communityAddress as Address}
-          registerToken={registerToken as Address}
-          registerStakeAmount={registerStakeAmount}
-        />
-        {/* pools */}
-      {/* <div
-          className={`flex transform flex-wrap items-center justify-center gap-4 overflow-hidden p-4 transition-height duration-200 ease-in-out ${
-            !open && "max-h-[290px]"
-          } `}
-        >
-          {pools.map((pool, i) => (
-            <PoolCard {...pool} key={i} />
-          ))}
-        </div>
-        {pools.length > 2 && (
-          <Button
-            // style="outline"
-            className="!rounded-full bg-white !p-3"
-            onClick={() => setOpen((prev) => !prev)}
-          >
-            <ChevronDownIcon
-              className={`block h-6 w-6 stroke-2 ${open && "rotate-180"}`}
-              aria-hidden="true"
-            />
-          </Button>
-        )}
-      </div> */}
     </>
   );
 }
