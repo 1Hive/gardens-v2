9963d6029e99c549cd48cab1dba7a88068e2b45b
Felipe Novaes F Rocha <windholyghost@gmail.com>
2024-04-09 20:46:28 -0300

    :zap: experimenting diff point config to deploy

--- pkg/contracts/script/DeployCV.s.sol
+++ pkg/contracts/script/DeployCV.s.sol
@@ -183,8 +183,8 @@ contract DeployCV is Native, CVStrategyHelpers, Script, SafeSetup {
         StrategyStruct.PointSystemConfig memory pointConfig;
         pointConfig.maxAmount = MINIMUM_STAKE * 2 * (10 ** 4);
         pointConfig.pointsPerMember = MINIMUM_STAKE * (10 ** 4);
-        pointConfig.tokensPerPoint = 1 * (DECIMALS);
-        pointConfig.pointsPerTokenStaked = 1 * (10 ** 4);
+        pointConfig.tokensPerPoint = 1 * DECIMALS;
+        pointConfig.pointsPerTokenStaked = 1 * DECIMALS * (10 ** 4);
         return pointConfig;
     }
 
