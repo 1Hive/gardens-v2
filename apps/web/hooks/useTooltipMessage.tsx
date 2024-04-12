2929b4ef69a7a4bc2f944752d714f2765f0fcbe7
Matias <mantitaaa89@gmail.com>
2024-04-12 18:35:27 -0300

    FIX: fix continue :  - handle missmatch network connection to `useTooltipMessage` hook for disable Button state

--- apps/web/hooks/useTooltipMessage.tsx
+++ apps/web/hooks/useTooltipMessage.tsx
@@ -1,21 +1,37 @@
+import { usePathname } from "next/navigation";
 import { useState, useEffect } from "react";
-import { useAccount } from "wagmi";
+import { useAccount, useChainId, useNetwork } from "wagmi";
 
 export interface ConditionObject {
   condition?: boolean;
   message: string;
 }
 
+const supportedChains: { [key: number]: string } = {
+  421614: "Arbitrum Sepolia",
+  1337: "Localhost",
+};
+
 export function useTooltipMessage(conditions: ConditionObject[]): string {
   const { isConnected } = useAccount();
+  const path = usePathname();
+  const urlChainId = Number(path.split("/")[2]); //  chain id from the url
+  const { chain } = useNetwork(); // wallet connected chain object
+  const missmatchUrlAndWalletConnectedNetwork = chain?.id !== urlChainId;
+
   const [tooltipMessage, setTooltipMessage] = useState<string>("");
 
   useEffect(() => {
-    const generateMessage = (isConnected: boolean): string => {
+    const generateMessage = (
+      isConnected: boolean,
+      isMissmatchConnection: boolean,
+    ): string => {
       if (!isConnected) {
         return "Connect Wallet";
       }
-
+      if (isMissmatchConnection) {
+        return `Switch to ${supportedChains[urlChainId] ?? ""} Network`;
+      }
       // Find the first active condition and return its message
       const activeCondition = conditions.find((cond) => cond.condition);
       if (activeCondition) {
@@ -24,13 +40,16 @@ export function useTooltipMessage(conditions: ConditionObject[]): string {
       return "tooltip message";
     };
 
-    const currentTooltipMessage = generateMessage(isConnected);
+    const currentTooltipMessage = generateMessage(
+      isConnected,
+      missmatchUrlAndWalletConnectedNetwork,
+    );
     setTooltipMessage(currentTooltipMessage);
 
     return () => {
       setTooltipMessage("");
     };
-  }, [conditions, isConnected]);
+  }, [conditions, isConnected, missmatchUrlAndWalletConnectedNetwork]);
 
   return tooltipMessage;
 }
