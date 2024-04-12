81bc5faf3121833fad06b331ef840f7723ac1ffc
Luciano Scaminaci <scaminaci.luciano@gmail.com>
2024-04-12 18:35:26 -0300

    refactor: FormInput

--- apps/web/components/Forms/FormInput.tsx
+++ apps/web/components/Forms/FormInput.tsx
@@ -7,38 +7,66 @@ import {
   UseFormRegister,
 } from "react-hook-form";
 
-type FormInputProps<T extends FieldValues> = {
+type Props = {
+  label: string;
+  subLabel?: string | undefined;
   type: HTMLInputTypeAttribute;
-  registerKey: Path<T>;
-  placeholder: string;
-  register: UseFormRegister<T>;
-  required?: boolean | string;
+  registerKey: any;
+  placeholder?: string;
+  register: any;
+  errors: any;
+  required?: boolean;
   registerOptions?: RegisterOptions;
+  children?: any;
+  rows?: number;
 };
 
-//TODO: handle errors for each input
-
-export const FormInput = <T extends FieldValues>({
+export function FormInput({
+  label = "",
+  subLabel,
   type,
   registerKey,
   placeholder = "",
   register,
+  errors,
   required = false,
   registerOptions,
-}: FormInputProps<T>) => (
-  <>
-    <input
-      type={type}
-      className="input input-bordered input-info w-full max-w-md"
-      placeholder={placeholder}
-      {...register(registerKey, {
-        required,
-        ...registerOptions,
-      })}
-    />
-
-    {/* {errors[registerKey] && (
-        <p className="text-red-500">{errors[registerKey]?.message}</p>
-      )} */}
-  </>
-);
+  children,
+  rows,
+}: Props) {
+  return (
+    <>
+      <label htmlFor={registerKey} className="my-2 text-lg text-black">
+        {label}
+      </label>
+      {subLabel && <p className="text-xs">{subLabel}</p>}
+      <div className={`relative ${type !== "textarea" && "max-w-md"}`}>
+        {type !== "textarea" ? (
+          <input
+            type={type}
+            placeholder={placeholder}
+            className={`hide-input-arrows input input-bordered input-${!!errors[registerKey] ? "error" : "info"} w-full`}
+            required
+            {...register(registerKey, {
+              required,
+              ...registerOptions,
+            })}
+          />
+        ) : (
+          <textarea
+            placeholder={placeholder}
+            className="textarea textarea-info line-clamp-5 w-full"
+            required
+            rows={rows}
+            {...register(registerKey, {
+              required,
+              ...registerOptions,
+            })}
+          />
+        )}
+        {children}
+      </div>
+      <p className="mt-2 text-sm text-red">{errors[registerKey]?.message || ""}</p>
+    </>
+  );
+}
