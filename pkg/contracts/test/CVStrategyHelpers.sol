52cf06f4f714ae0b17f09eab587a4cbf664e06ba
Felipe Novaes F Rocha <windholyghost@gmail.com>
2024-05-28 15:29:23 -0300

    :zap: fix CI

--- pkg/contracts/test/CVStrategyHelpers.sol
+++ pkg/contracts/test/CVStrategyHelpers.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.19;
 
 import "forge-std/console.sol";
 import {Allo} from "allo-v2-contracts/core/Allo.sol";
