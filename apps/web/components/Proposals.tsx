244e567fe40d8781ae1c53e6885a7d5beed10e0a
Lucianosc <scaminaci.luciano@gmail.com>
2024-05-31 13:39:02 -0300

    fix disable Save changes button when no changes in proposals

--- apps/web/components/Proposals.tsx
+++ apps/web/components/Proposals.tsx
@@ -30,6 +30,7 @@ import { queryByChain } from "@/providers/urql";
 import { getChainIdFromPath } from "@/utils/path";
 import { useDisableButtons, ConditionObject } from "@/hooks/useDisableButtons";
 import { useUrqlClient } from "@/hooks/useUqrlClient";
+import { toast } from "react-toastify";
 
 export type ProposalInputItem = {
   id: string;
@@ -218,14 +219,20 @@ export function Proposals({
   }, [allocateStatus]);
 
   const submit = async () => {
-    const encodedData = getEncodedProposals(inputs, stakedFilters);
+    const proposalsDifferencesArr = getProposalsInputsDifferences(
+      inputs,
+      stakedFilters,
+    );
+    const encodedData = encodeFunctionParams(cvStrategyABI, "supportProposal", [
+      proposalsDifferencesArr,
+    ]);
     const poolId = Number(strategy.poolId);
     writeAllocate({
       args: [BigInt(poolId), encodedData as AddressType],
     });
   };
 
-  const getEncodedProposals = (
+  const getProposalsInputsDifferences = (
     inputData: ProposalInputItem[],
     currentData: ProposalInputItem[],
   ) => {
@@ -239,15 +246,11 @@ export function Proposals({
           row = [Number(input.id), dif];
         }
       });
-      if (!!row) resultArr.push(row);
+      if (!!row && row[1] !== 0n) resultArr.push(row);
     });
 
-    const encodedData = encodeFunctionParams(cvStrategyABI, "supportProposal", [
-      resultArr,
-    ]);
-    return encodedData;
+    return resultArr;
   };
-
   const calculateTotalTokens = (exceptIndex?: number) =>
     inputs.reduce((acc, curr, i) => {
       if (exceptIndex !== undefined && exceptIndex === i) return acc;
@@ -257,10 +260,15 @@ export function Proposals({
   const inputHandler = (i: number, value: number) => {
     const currentPoints = calculateTotalTokens(i);
     const maxAllowableValue = memberActivatedPoints - currentPoints;
-
+    const toastId = "error-toast";
     // If the sum exceeds the memberActivatedPoints, adjust the value to the maximum allowable value
     if (currentPoints + value > memberActivatedPoints) {
       value = maxAllowableValue;
+      if (!toast.isActive(toastId)) {
+        toast.error("Can't exceed 100% in total support!", {
+          toastId,
+        });
+      }
       console.log("can't exceed 100% points");
     }
 
@@ -308,8 +316,8 @@ export function Proposals({
 
   // console.log("newLocal:                    %s", memberActivatePointsAsNum);
   // console.log("newLocal_1:                  %s", totalEAPasNum);
-  console.log("memberActivatedPoints:       %s", memberActivatedPoints);
-  console.log("memberPoolWeight:            %s", memberPoolWeight);
+  // console.log("memberActivatedPoints:       %s", memberActivatedPoints);
+  // console.log("memberPoolWeight:            %s", memberPoolWeight);
   // console.log(
   //   "totalEffectiveActivePoints:  %s",
   //   strategy.totalEffectiveActivePoints,
@@ -416,8 +424,10 @@ export function Proposals({
                   className="min-w-[200px]"
                   onClick={() => submit()}
                   isLoading={allocateStatus === "loading"}
-                  disabled={inputAllocatedTokens > memberActivatedPoints}
-                  tooltip="Assigned points can't exceed total activated points pool"
+                  disabled={
+                    !getProposalsInputsDifferences(inputs, stakedFilters).length
+                  }
+                  tooltip="Make changes in proposals support first"
                 >
                   Save changes
                 </Button>
