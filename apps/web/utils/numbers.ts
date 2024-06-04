ecd5b8344453b400a9d42dc889043d45322bb5d6
Lucianosc <scaminaci.luciano@gmail.com>
2024-06-03 23:22:03 -0300

    break down SCALE_PRECISION into CV_PERCENTAGE_SCALE & UI_PERCENTAGE_FORMAT

--- apps/web/utils/numbers.ts
+++ apps/web/utils/numbers.ts
@@ -3,8 +3,16 @@ import { formatUnits } from "viem";
 
 export const INPUT_MIN_VALUE = 0.000000000001;
 export const MAX_RATIO_CONSTANT = 0.77645;
-export const SCALE_PRECISION = 10 ** 6;
-export const SCALE_PRECISION_DECIMALS = 6;
+export const CV_PERCENTAGE_SCALE = 10 ** 4;
+export const CV_PERCENTAGE_SCALE_DECIMALS = 4;
+
+export const UI_PERCENTAGE_FORMAT = 10 ** 2; // 100% = 1
+export const UI_PERCENTAGE_FORMAT_DECIMALS = 2;
+
+export const SCALE_PRECISION = CV_PERCENTAGE_SCALE * UI_PERCENTAGE_FORMAT; // 1% = 10.000
+export const SCALE_PRECISION_DECIMALS =
+  CV_PERCENTAGE_SCALE_DECIMALS + UI_PERCENTAGE_FORMAT_DECIMALS; // 6 decimals
+
 export const CV_SCALE_PRECISION = 10 ** 7;
 export const CV_SCALE_PRECISION_DECIMALS = 7;
 
