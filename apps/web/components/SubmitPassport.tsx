9da98703bdfb43c0267293ea84f5e8c52983b5ab
rperez89 <rodrigopi1989@gmail.com>
2024-06-12 23:20:39 -0300

    submit passport button

---
+++ apps/web/components/SubmitPassport.tsx
@@ -0,0 +1,101 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import { Button } from "./Button";
+import { useAccount, useSignMessage } from "wagmi";
+import { cvStrategyABI, registryCommunityABI } from "@/src/generated";
+import useErrorDetails from "@/utils/getErrorName";
+import { abiWithErrors } from "@/utils/abiWithErrors";
+import { useConnectModal } from "@rainbow-me/rainbowkit";
+import { useTransactionNotification } from "@/hooks/useTransactionNotification";
+import { useDisableButtons, ConditionObject } from "@/hooks/useDisableButtons";
+import { connect } from "http2";
+import { Data } from "@thirdweb-dev/chain-icons";
+
+export function SubmitPassport() {
+  const { address: connectedAccount } = useAccount();
+  const { openConnectModal } = useConnectModal();
+  const [nonce, setNonce] = useState<string | null>(null);
+
+  async function handleOnClick() {
+    if (connectedAccount) {
+    } else {
+      openConnectModal?.();
+    }
+  }
+
+  const getSigningMessage = async () => {
+    const SIGNING_MESSAGE_URI = "/api/passport/signMessage";
+
+    try {
+      const response = await fetch(SIGNING_MESSAGE_URI, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      const json = await response.json();
+
+      console.log("Signing message response", json);
+      setNonce(json.nonce);
+      return json.message;
+    } catch (err) {
+      console.log("error: ", err);
+      return null;
+    }
+  };
+
+  const submitSignedPassport = async (
+    address: string,
+    signature: string,
+    nonce: string,
+  ) => {
+    const SUBMIT_SIGNED_PASSPORT_URI = "/api/passport/submitPassport";
+
+    try {
+      const response = await fetch(SUBMIT_SIGNED_PASSPORT_URI, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ address, signature, nonce }),
+      });
+
+      const data = await response.json();
+      console.log("Response from server:", data);
+    } catch (err) {
+      console.error("Error submitting signed passport:", err);
+    }
+  };
+
+  const { signMessage } = useSignMessage({
+    onSuccess: async (data, variables) => {
+      console.log("variables ", variables);
+      console.log("signature ", data);
+      if (connectedAccount && nonce) {
+        await submitSignedPassport(connectedAccount, data, nonce);
+      }
+    },
+  });
+
+  const handleSignMessage = async () => {
+    if (!connectedAccount) {
+      alert("Please connect your wallet first");
+      return;
+    }
+
+    const message = await getSigningMessage();
+    console.log("Signing message:", message);
+    if (message) {
+      signMessage({ message });
+    }
+  };
+  return (
+    <>
+      <div className="flex flex-col gap-4 pl-4">
+        <Button onClick={handleSignMessage} className="w-fit bg-primary">
+          Submit Passport
+        </Button>
+      </div>
+    </>
+  );
+}
