9e85f13f1dee06ac532550b47b355cb2c682d529
Corantin <corantin.noll@gmail.com>
2024-06-17 21:01:37 -0400

    Try make PR more readable

--- apps/web/app/(app)/gardens/[chain]/[garden]/page.tsx
+++ apps/web/app/(app)/gardens/[chain]/[garden]/page.tsx
@@ -1,17 +1,14 @@
-"use client";
-
 import { tree2, tree3, grassLarge } from "@/assets";
 import Image from "next/image";
-import { CommunityCard, FormLink } from "@/components";
+import { CommunityCard } from "@/components";
 import {
-  RegistryCommunity,
   TokenGarden,
   getCommunitiesByGardenDocument,
   getCommunitiesByGardenQuery,
 } from "#/subgraph/.graphclient";
-
-import { useEffect, useState } from "react";
+import { FormLink } from "@/components";
 import useSubgraphQueryByChain from "@/hooks/useSubgraphQueryByChain";
+import { useEffect, useState } from "react";
 
 export const dynamic = "force-dynamic";
 
@@ -67,47 +64,45 @@ export default async function Garden({
 
   return (
     <div className="relative mx-auto max-w-6xl space-y-10 rounded-xl border-2 border-black bg-base-100 bg-surface p-8">
-      {tokenGarden ?
-        <>
-          <header className="relative flex min-h-[500px] flex-col items-center justify-between gap-6 px-3">
-            <div className="flex h-full min-h-96 flex-col items-center justify-between p-1">
-              <h3 className="text-center font-press">
-                {tokenGarden?.symbol} Token Ecosystem
-              </h3>
-              <p className="max-w-lg text-center font-semibold leading-7">
-                Discover communities in the
-                <span className="text-primary"> {tokenGarden?.name} Garden</span>,
-                where you connect with people and support proposals bounded by a
-                shared
-                <span className="text-primary"> covenant.</span>
-              </p>
-              <FormLink
-                label="Create Community"
-                href={`/gardens/${chain}/${garden}/create-community`}
-              />
-            </div>
-            <Image src={tree2} alt="tree" className="absolute bottom-0 left-5" />
-            <Image src={tree3} alt="tree" className="absolute bottom-0 right-5" />
-            <Image
-              src={grassLarge}
-              alt="grass"
-              className="absolute -bottom-1 h-10 overflow-hidden"
-            />
-          </header>
-          <section className="mx-auto flex flex-col gap-8">
-            <h4 className="rounded-b-xl bg-surface py-6 text-center font-press shadow">
-              {tokenGarden?.name} Communities
-            </h4>
-            {communities?.map((community, i) => (
-              <CommunityCard
-                {...community}
-                tokenGarden={tokenGarden as TokenGarden}
-                key={`${community.communityName}_${i}`}
-              />
-            ))}
-          </section>
-        </>
-        : <div className="w-full text-center"><div className="loading"></div></div>}
+      <header className="relative flex min-h-[500px] flex-col items-center justify-between gap-6 px-3">
+        <div className="flex h-full min-h-96 flex-col items-center justify-between p-1">
+          <h3 className="text-center font-press">
+            {tokenGarden?.symbol} Token Ecosystem
+          </h3>
+          <p className="max-w-lg text-center font-semibold leading-7">
+            Discover communities in the
+            <span className="text-primary"> {tokenGarden?.name} Garden</span>,
+            where you connect with people and support proposals bounded by a
+            shared
+            <span className="text-primary"> covenant.</span>
+          </p>
+          <FormLink
+            label="Create Community"
+            href={`/gardens/${chain}/${garden}/create-community`}
+          />
+        </div>
+        <Image src={tree2} alt="tree" className="absolute bottom-0 left-5" />
+        <Image src={tree3} alt="tree" className="absolute bottom-0 right-5" />
+        <Image
+          src={grassLarge}
+          alt="grass"
+          className="absolute -bottom-1 h-10 overflow-hidden"
+        />
+      </header>
+      <section className="mx-auto flex flex-col gap-8">
+        <h4 className="rounded-b-xl bg-surface py-6 text-center font-press shadow">
+          {tokenGarden?.name} Communities
+        </h4>
+
+        {/* communites */}
+        {communities?.map((community, i) => (
+          <CommunityCard
+            {...community}
+            tokenGarden={tokenGarden as TokenGarden}
+            key={`${community.communityName}_${i}`}
+          />
+        ))}
+      </section>
     </div>
   );
 }
