d55e151d536f30aa80c72941f6653846c20a8375
Matias <mantitaaa89@gmail.com>
2024-06-20 23:40:08 -0300

    feat: match daisyUI color names with our figma design color system

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -7,53 +7,55 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   daisyui: {
-    themes: false, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
-    darkTheme: false, // name of one of the included themes for dark mode
-    base: true, // applies background color and foreground color for root element by default
-    styled: true, // include daisyUI colors and design decisions for all components
-    utils: true, // adds responsive and modifier utility classes
-    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
-    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
-    themeRoot: ":root", // The element that receives theme color CSS variables
+    themes: [
+      {
+        lightTheme: {
+          primary: "#65ad18",
+          "primary-content": "#65ad18",
+          secondary: "#fff4e6",
+          "secondary-content": "#ff9500",
+          neutral: "#acacac",
+          "neutral-content": "#000000",
+          "base-100": "#ffffff",
+          "base-200": "#f1f1f1",
+          "base-300": "#eeeeee",
+          "base-content": "#ffffff",
+          info: "#e5f7fa",
+          "info-content": "#2baac7",
+          error: "#eb4848",
+          "error-content": "#eb4848",
+          //additions:
+          "neutral-content-500": "#8c8c8c",
+          "primary-hover": "#3e920f",
+          "primary-50": "#c7eb9a",
+          "primary-25": "#e2f5cb",
+          "error-hover": "#d03a3a",
+          "error-100": "#ffcfcf",
+        },
+      },
+    ],
+    darkTheme: false,
+    base: true,
+    styled: true,
+    utils: true,
+    prefix: "",
+    logs: true,
+    themeRoot: ":root",
   },
   theme: {
-    content: [""],
     extend: {
       fontFamily: {
         press: ["var(--font-inter)"],
         chakra: ["var(--font-chakra)"],
       },
       colors: {
-        primary: "var(--color-green-500)",
-        secondary: "var(--color-orange-500)",
-        surface: "var(--color-surface)",
-        white: "var(--color-white)",
-        black: "var(--color-black)",
-        accent: "var(--color-accent)",
-        red: "var(--color-red)",
-        // new light color palette:
-        neutral: "var(--color-grey-900)",
-        neutralSoft: "var(--color-grey-500)",
-        neutralButtonBg: "var(--color-grey-400)",
-        neutralSoftBg: "var(--color-grey-200)",
-        neutralSoft2Bg: "var(--color-grey-100)",
-        neutralInverted: "var(--color-grey-0)",
-        neutralBg: "var(--color-grey-0)",
-        //
-        primaryHover: "var(--color-green-600)",
-        primaryButtonBg: "var(--color-green-500)",
-        primarySoftBg: "var(--color-green-50)",
-        primaryBg: "var(--color-green-25)",
-        //
-        warn: "var(--color-red-500)",
-        warnHover: "var(--color-red-600)",
-        warnButtonBg: "var(--color-red-500)",
-        warnSoftBg: "var(--color-red-100)",
-        //
-        secondarySoftBg: "var(--color-orange-50)",
-        //
-        terciary: "var(--color-cyan-500)",
-        terciarySoftBg: "var(--color-cyan-50)",
+        //added color for the our daisy - lightTheme
+        "neutral-content-500": "#8c8c8c",
+        "primary-hover": "#3e920f",
+        "primary-50": "#c7eb9a",
+        "primary-25": "#e2f5cb",
+        "error-hover": "#d03a3a",
+        "error-100": "#ffcfcf",
       },
       transitionProperty: {
         height: "height",
