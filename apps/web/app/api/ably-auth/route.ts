d86787ca30032e5516e71f0aae690e840e6a34eb
Corantin <corantin.noll@gmail.com>
2024-06-21 15:54:41 -0400

    Ensure cors

--- apps/web/app/api/ably-auth/route.ts
+++ apps/web/app/api/ably-auth/route.ts
@@ -4,8 +4,19 @@ import { NextResponse, NextRequest } from "next/server";
 
 const ably = new Ably.Rest({ key: process.env.NEXT_ABLY_API_KEY });
 
+const allowedOrigins = [process.env.VERCEL_URL];
+
 export async function GET(req: NextRequest, res: NextResponse) {
   try {
+    const origin = req.headers.get("origin") ?? "";
+    const isAllowedOrigin = allowedOrigins.includes(origin);
+    if (!isAllowedOrigin) {
+      return NextResponse.json(
+        { error: "Origin not allowed" },
+        { status: 403 },
+      );
+    }
+
     const tokenRequestData = {
       capability: JSON.stringify({ "*": ["publish", "subscribe", "presence"] }), // Adjust based on your needs
     };
