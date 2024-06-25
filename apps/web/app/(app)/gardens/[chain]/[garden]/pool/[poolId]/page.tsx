858aa8d12c5fe4fadb44c414543af3a0538cb325
Matias <mantitaaa89@gmail.com>
2024-06-25 16:11:07 -0300

    fix: improve readability

--- apps/web/app/(app)/gardens/[chain]/[garden]/pool/[poolId]/page.tsx
+++ apps/web/app/(app)/gardens/[chain]/[garden]/pool/[poolId]/page.tsx
@@ -87,7 +87,7 @@ export default async function Pool({
                         Conviction Voting
                       </span>
                     </div>
-                    {proposalType == 1 && (
+                    {poolTypes[proposalType] !== "sigaling" && (
                       <div className="text-md stat-title">
                         Funding Token:{" "}
                         <span className="text-md pl-2 text-black">
