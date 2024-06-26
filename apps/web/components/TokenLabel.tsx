5cc1cf396aa3c2a9cf15800fa8c137de80699600
Lucianosc <scaminaci.luciano@gmail.com>
2024-06-25 21:38:15 -0300

    minor fix TokenLAbel

--- apps/web/components/TokenLabel.tsx
+++ apps/web/components/TokenLabel.tsx
@@ -4,9 +4,8 @@ import React from "react";
 type TokenLabelProps = { chainId: string | number };
 
 export function TokenLabel({ chainId }: TokenLabelProps) {
-  console.log(chainId)
   return (
-    <div className="align-center flex rounded-full bg-neutral-soft-2 px-4 py-2">
+    <div className="align-center flex h-fit w-fit justify-center rounded-full bg-neutral-soft-2 px-4 py-2">
       <div className="flex content-center justify-center">
         {/* TODO: change Icon library */}
         <ChainIcon chain={chainId} height={19} />
