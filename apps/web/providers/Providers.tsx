83cfb8b546a82b46007565de0426cb37b040a676
Lucianosc <scaminaci.luciano@gmail.com>
2024-07-02 19:44:07 -0300

    wip update provider

--- apps/web/providers/Providers.tsx
+++ apps/web/providers/Providers.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "@rainbow-me/rainbowkit/styles.css";
 import ThemeProvider from "./ThemeProvider";
 import {
@@ -13,11 +13,18 @@ import {
   injectedWallet,
   coinbaseWallet,
 } from "@rainbow-me/rainbowkit/wallets";
-import { createConfig, WagmiConfig } from "wagmi";
-import { chains, publicClient } from "@/configs/wagmiConfig";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import {
+  chains,
+  publicClient as wagmiPublicClient,
+} from "@/configs/wagmiConfig";
 import { AddrethConfig } from "addreth";
 import UrqlProvider from "./UrqlProvider";
 import { PubSubProvider } from "@/contexts/pubsub.context";
+import { getChainIdFromPath } from "@/utils/path";
+import { getChain } from "@/configs/chainServer";
+import { publicProvider } from "wagmi/providers/public";
+import { alchemyProvider } from "wagmi/providers/alchemy";
 
 type Props = {
   children: React.ReactNode;
@@ -35,15 +42,38 @@ const connectors = connectorsForWallets([
   },
 ]);
 
-export const wagmiConfig = createConfig({
-  autoConnect: true,
-  connectors,
-  publicClient,
-});
-
 const Providers = ({ children }: Props) => {
-  const [mounted, setMounted] = React.useState(false);
-  React.useEffect(() => setMounted(true), []);
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => setMounted(true), []);
+  const [wagmiConfig, setWagmiConfig] =
+    useState<ReturnType<typeof createConfig>>();
+
+  useEffect(() => {
+    const chainId = getChainIdFromPath();
+    const chain = getChain(chainId);
+    const publicClient = chain
+      ? configureChains(
+          [chain],
+          [
+            publicProvider(),
+            alchemyProvider({
+              apiKey: process.env.NEXT_PUBLIC_RPC_URL_ARB_TESTNET || "",
+            }),
+          ],
+        ).publicClient
+      : wagmiPublicClient;
+
+      const wagmiConfig =  createConfig({
+        autoConnect: true,
+        connectors,
+        publicClient,
+      });
+
+    setWagmiConfig(
+      wagmiConfig
+    );
+
+  }, []);
 
   return (
     // if mounted UrlqProvider will be rendered
