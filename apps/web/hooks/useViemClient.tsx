6f9745d434ff8e8a5cffda6e4170e74680a4b78d
Corantin <corantin.noll@gmail.com>
2024-07-04 14:23:50 -0400

    Fix gardens oage where no chainId from path

--- apps/web/hooks/useViemClient.tsx
+++ apps/web/hooks/useViemClient.tsx
@@ -1,10 +1,14 @@
 import { useEffect, useState } from "react";
 import { PublicClient, createPublicClient, http } from "viem";
+import { useChainId } from "wagmi";
+import { getChain } from "@/configs/chainServer";
 import useChainFromPath from "./useChainIdFromtPath";
 
 export const useViemClient = function () {
-  const chain = useChainFromPath();
-
+  const chainFromPath = useChainFromPath();
+  const chainId = useChainId();
+  const chain = chainFromPath ?? getChain(chainId);
+  
   const [viemClient, setViemClient] = useState<PublicClient>(
     createPublicClient({
       chain,
