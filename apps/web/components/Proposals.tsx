6c29dcc69402fd7df9eb43fbfb9269bbc1ba762a
Mati <94074651+Mati0x@users.noreply.github.com>
2024-07-09 12:56:11 -0300

    Update apps/web/components/Proposals.tsx
    Co-authored-by: Corantin <corantin.noll@gmail.com>

--- apps/web/components/Proposals.tsx
+++ apps/web/components/Proposals.tsx
@@ -215,7 +215,7 @@ export function Proposals({
   }, [proposals, address, stakedFilters]);
 
   useEffect(() => {
-    if (isMemberActived === undefined) return;
+    if (isMemberActived == null) return;
     if (isMemberActived !== true) setAllocationView(false);
   }, [isMemberActived]);
 
