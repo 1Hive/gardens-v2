5279472e50d0dd4bd01a31dc2faf77584664f2e8
Corantin <corantin.noll@gmail.com>
2024-07-12 18:52:42 -0400

    Update layout.tsx

--- apps/web/app/(app)/layout.tsx
+++ apps/web/app/(app)/layout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { GoBackButton, NavBar } from "@/components";
 import { Breadcrumbs } from "@/components/Breadcrumbs";
 
-export function Layout({ children }: { children: React.ReactNode }) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <NavBar />
