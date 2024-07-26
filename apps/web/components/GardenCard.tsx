6daa48edc6fee902360405defd8b49d270e990e1
Matias <mantitaaa89@gmail.com>
2024-07-26 20:46:40 -0300

    refactor: new `GardenCard` style implementation + add noSymbol prop upgrade to `TokenLabel`

--- apps/web/components/GardenCard.tsx
+++ apps/web/components/GardenCard.tsx
@@ -1,16 +1,17 @@
 "use client";
 
+import { BuildingStorefrontIcon } from "@heroicons/react/24/outline";
 import Image from "next/image";
-import Link from "next/link";
 import { getTokenGardensQuery } from "#/subgraph/.graphclient";
-import { Button } from ".";
+import { Statistic, TokenLabel } from ".";
 import { gardenLand } from "@/assets";
-import { ChainIcon, getChain } from "@/configs/chainServer";
+import { Card } from "@/components/Card";
+import { getChain } from "@/configs/chainServer";
 
 type TokenGarden = getTokenGardensQuery["tokenGardens"][number];
 
 export function GardenCard({ garden }: { garden: TokenGarden }) {
-  const { id, name, symbol, chainId } = garden;
+  const { id, name, chainId } = garden;
   const link = `/gardens/${chainId}/${id}`;
   const communities = garden.communities?.filter((comm) => comm.isValid);
   const commLength = communities?.length ?? 0;
@@ -19,44 +20,30 @@ export function GardenCard({ garden }: { garden: TokenGarden }) {
       ?.map((comm) => comm.members?.length ?? 0)
       .reduce((a, b) => a + b, 0) ?? 0; //@todo temporary, that can be take from the subgraph
   return (
-    <div className="border2 bg-surface relative flex max-w-[320px] flex-col overflow-hidden rounded-lg border-black shadow">
-      <div className="flex flex-col gap-2 p-2">
-        <div className="card relative" />
-        <div className="flex flex-col items-center gap-1">
-          <h4 className="text-2xl font-bold">{name}</h4>
-          <p className="text-center">{symbol}</p>
-          <div className="align-center flex justify-center">
-            <p className="text-center text-xs">Network:</p>
-            <div className="mx-1 content-center justify-center">
-              <ChainIcon chain={chainId} height={16} />
+    <Card href={link} >
+      <div className="flex flex-col gap-7">
+        <div className="flex items-center justify-center gap-2">
+          {/* TODO: find appropiate token image */}
+          <TokenLabel chainId={chainId} noSymbol />
+          <h3 className="text-neutral-content">{name}</h3>
+        </div>
+        <div className="flex flex-col gap-4">
+          <div className="flex flex-col">
+            <div className="align-start flex flex-col justify-start">
+              <p className="text-neutral-content text-sm">Network:</p>
+              <div className="flex gap-2.5 items-center">
+                <h5 className="text-neutral-content">{getChain(chainId)?.name}</h5>
+              </div>
             </div>
-            <p className="text-center text-xs">{getChain(chainId)?.name}</p>
+          </div>
+          <div className="flex flex-col gap-1">
+            <Statistic label={`Communit${commLength > 1 ? "ies" : "y"}`} count={commLength} icon={<BuildingStorefrontIcon />} />
+            <Statistic label={"Members"} count={totalMembers} />
           </div>
         </div>
-        <InfoData
-          label={`Communit${commLength > 1 ? "ies" : "y"}`}
-          data={commLength}
-        />
-        <InfoData label={"Members"} data={totalMembers} />
-
-        <div className="mb-2 mt-4">
-          <Link href={link}>
-            <Button className="w-full">View Communities</Button>
-          </Link>
-        </div>
+        <Image src={gardenLand} alt="garden land" className="rounded-xl" />
       </div>
-      <Image src={gardenLand} alt="garden land" />
-    </div>
+    </Card>
   );
 }
 
-const InfoData = ({ label, data }: { label: string; data: any }) => {
-  return (
-    <dl className="flex items-center justify-between rounded-xl px-3 shadow">
-      <dt className="truncate text-sm font-medium text-gray-500">{label}</dt>
-      <dd className="mt-1 max-w-10 overflow-hidden truncate text-2xl font-semibold tracking-tight text-gray-900">
-        {data}
-      </dd>
-    </dl>
-  );
-};
--- apps/web/components/TokenLabel.tsx
+++ apps/web/components/TokenLabel.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { ChainIcon, getChain } from "@/configs/chainServer";
 
-type TokenLabelProps = { chainId: string | number };
+type TokenLabelProps = { chainId: string | number, noSymbol?: boolean };
 
-export function TokenLabel({ chainId }: TokenLabelProps) {
+export function TokenLabel({ chainId, noSymbol = false }: TokenLabelProps) {
   return (
     <div className="align-center flex h-fit w-fit justify-center rounded-full bg-neutral-soft-2 px-4 py-2">
       <div className="flex content-center justify-center">
         {/* TODO: change Icon library */}
-        <ChainIcon chain={chainId} height={19} />
+        <ChainIcon chain={chainId} height={24} />
       </div>
-      <h6 className="ml-[10px]">{getChain(chainId)?.nativeCurrency.symbol}</h6>
+      {!noSymbol && <h6 className="ml-[10px]">{getChain(chainId)?.nativeCurrency.symbol}</h6> }
     </div>
   );
 }
