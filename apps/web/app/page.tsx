bd04ae4bef33a1b7b5b726d2122f0db18f597fc3
Matias <mantitaaa89@gmail.com>
2024-08-06 13:53:35 -0300

    feat(landing): move footer to center of page

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -128,13 +128,12 @@ const Hero = () => {
                 </div>
                 <div className="py-6">
                   <a
-                    href="https://la74xhyqvjj.typeform.com/to/ocrTRQFb"
-                    className="flex w-full items-center justify-center bg-neutral-inverted-content py-1 text-center text-sm font-semibold leading-6 hover:opacity-95"
+                    href="/gardens"
+                    className="flex items-center justify-center text-sm font-semibold leading-6 text-gray-900"
                     target="_blank"
                     rel="noreferrer"
                   >
-                    <span>Sign up </span>
-                    <ArrowLongRightIcon className="ml-2 h-5 w-5" />
+                    <Button>Launch App</Button>
                   </a>
                 </div>
               </div>
@@ -358,10 +357,10 @@ const SignUp = () => {
       <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
         <div className="relative isolate overflow-hidden bg-neutral-content px-6 py-24 shadow-2xl sm:rounded-3xl sm:px-24 xl:py-32">
           <h2 className="mx-auto max-w-2xl text-center text-3xl font-bold tracking-tight text-neutral sm:text-4xl">
-            Cultivate Change with Gardens
+            Cultivate change with Gardens
           </h2>
           <p className="mx-auto mt-2 max-w-xl text-center text-lg leading-8 text-neutral-soft">
-            Join communities, shape decisions, and grow the public economy.
+            Join our Beta and start growing your community.
           </p>
           <div className="mx-auto mt-10 flex max-w-md items-center justify-center gap-x-4 rounded-xl">
             {/* TODO: link to gardnes form */}
@@ -460,8 +459,8 @@ const navigation = [
 const Footer = () => {
   return (
     <footer className="bg-neutral">
-      <div className="mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8">
-        <div className="flex justify-center space-x-6 md:order-2">
+      <div className="mx-auto max-w-7xl px-6 py-12 md:flex md:flex-col md:items-center md:justify-between lg:px-8 gap-8">
+        <div className="flex justify-center space-x-6">
           {navigation.map((item) => (
             <a
               key={item.name}
@@ -475,7 +474,7 @@ const Footer = () => {
             </a>
           ))}
         </div>
-        <div className="mt-8 md:order-1 md:mt-0">
+        <div className="mt-8 md:mt-0">
           <p className="text-center text-xs leading-5 text-gray-500">
             &copy; 2024 Gardens. All rights reserved.
           </p>
