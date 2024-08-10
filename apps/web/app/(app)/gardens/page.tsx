8dc01657c635f117507f9811d602485098dd3b16
Matias <mantitaaa89@gmail.com>
2024-08-10 20:00:10 -0300

    feat: add new "clouds images" from latest ilustrations + try new bg in gardens page with hand made banner image

--- apps/web/app/(app)/gardens/page.tsx
+++ apps/web/app/(app)/gardens/page.tsx
@@ -6,7 +6,7 @@ import {
   getTokenGardensDocument,
   getTokenGardensQuery,
 } from "#/subgraph/.graphclient";
-import { clouds1, clouds2, gardenHeader } from "@/assets";
+import { clouds1, clouds2, Banner } from "@/assets";
 import { GardenCard } from "@/components";
 import { LoadingSpinner } from "@/components/LoadingSpinner";
 import { useSubgraphQueryMultiChain } from "@/hooks/useSubgraphQueryMultiChain";
@@ -55,34 +55,41 @@ export default function Page() {
   }, [fetching, tokenGardens?.length]);
 
   return (
-    <div className="flex flex-col items-center justify-center gap-8">
-      <header className="flex flex-col items-center gap-8">
-        <div className="flex items-center text-center">
-          <div className="relative flex-1">
-            <Image src={clouds1} alt="clouds" />
-          </div>
-          <div className="mx-10 flex flex-col items-center gap-5">
-            <div className="flex flex-col items-center">
-              <h1 className="max-w-xl text-center text-neutral-content">
-                Explore and Join Gardens Ecosystems
-              </h1>
-              <p className="text-xl text-primary-content">
-                A place where you help shape digital economies
-              </p>
+    <>
+      <div className="flex flex-col items-center justify-center gap-8 relative z-10">
+        <header className="flex flex-col items-center gap-8">
+          <div className="flex items-center text-center">
+            <div className="relative flex-1">
+              <Image src={clouds1} alt="clouds" />
+            </div>
+            <div className="mx-10 flex flex-col items-center gap-5">
+              <div className="flex flex-col items-center">
+                <h1 className="max-w-xl text-center text-neutral-content">
+                  Explore and Join Gardens Ecosystems
+                </h1>
+                <p className="text-xl text-primary-content">
+                  A place where you help shape digital economies
+                </p>
+              </div>
+            </div>
+            <div className="relative flex-1">
+              <Image src={clouds2} alt="clouds" />
             </div>
           </div>
-          <div className="relative flex-1">
-            <Image src={clouds2} alt="clouds" />
+          <div className="relative" />
+        </header>
+        <section className="my-2 flex w-full max-w-2xl flex-col items-center justify-center gap-8 ">
+          <div className="grid max-w-7xl grid-cols-[repeat(auto-fit,minmax(310px,1fr))] gap-6 md:grid-cols-[repeat(auto-fit,minmax(320px,1fr))] z-10">
+            {GardenList}
           </div>
-        </div>
-        <div className="relative" />
-      </header>
-      <section className="my-2 flex w-full max-w-2xl flex-col items-center justify-center gap-8">
-        <div className="grid max-w-7xl grid-cols-[repeat(auto-fit,minmax(310px,1fr))] gap-6 md:grid-cols-[repeat(auto-fit,minmax(320px,1fr))]">
-          {GardenList}
-        </div>
-      </section>
-      {/* <Image src={gardenHeader} alt="gardens" /> */}
-    </div>
+        </section>
+      </div>
+      <Image
+        src={Banner}
+        alt="gardens"
+        className="absolute inset-0 object-cover w-full h-full"
+        loading="lazy"
+      />
+    </>
   );
 }
