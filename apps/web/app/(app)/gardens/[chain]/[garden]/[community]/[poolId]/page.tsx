5c4739cdda58ee30d500fd9c381ec5f33dbe3811
Lucianosc <scaminaci.luciano@gmail.com>
2024-08-10 13:27:45 -0300

    refactor: Pool page

--- apps/web/app/(app)/gardens/[chain]/[garden]/[community]/[poolId]/page.tsx
+++ apps/web/app/(app)/gardens/[chain]/[garden]/[community]/[poolId]/page.tsx
@@ -2,40 +2,20 @@
 
 import { useEffect, useState } from "react";
 import {
-  BoltIcon,
-  ChartBarIcon,
-  ClockIcon,
-  Square3Stack3DIcon,
-} from "@heroicons/react/24/outline";
-import Image from "next/image";
-import {
   getAlloQuery,
   getPoolDataDocument,
   getPoolDataQuery,
 } from "#/subgraph/.graphclient";
 import { Address } from "#/subgraph/src/scripts/last-addr";
-import { blueLand, grassLarge } from "@/assets";
-import {
-  Badge,
-  InfoIcon,
-  PoolMetrics,
-  Proposals,
-  Statistic,
-} from "@/components";
+import { PoolMetrics, Proposals } from "@/components";
 import { LoadingSpinner } from "@/components/LoadingSpinner";
 import PoolHeader from "@/components/PoolHeader";
-import { chainDataMap } from "@/configs/chainServer";
 import { QUERY_PARAMS } from "@/constants/query-params";
 import { useCollectQueryParams } from "@/hooks/useCollectQueryParams";
-import { useDisableButtons } from "@/hooks/useDisableButtons";
 import { useSubgraphQuery } from "@/hooks/useSubgraphQuery";
-import { pointSystems, poolTypes } from "@/types";
+import { poolTypes } from "@/types";
 import { getIpfsMetadata } from "@/utils/ipfsUtils";
-import {
-  CV_SCALE_PRECISION,
-  formatTokenAmount,
-  MAX_RATIO_CONSTANT,
-} from "@/utils/numbers";
+import { CV_SCALE_PRECISION } from "@/utils/numbers";
 
 export const dynamic = "force-dynamic";
 
@@ -120,136 +100,29 @@ export default function Page({
     return <div className="mt-52 text-center">Pool {poolId} not found</div>;
   }
 
-  const pointSystem = data.cvstrategies?.[0].config?.pointSystem;
+  const pointSystem = data.cvstrategies?.[0].config.pointSystem;
   const communityAddress = strategyObj.registryCommunity.id as Address;
   const alloInfo = data.allos[0];
-  const proposalType = strategyObj?.config?.proposalType as number;
-  const poolAmount = strategyObj?.poolAmount as number;
+  const proposalType = strategyObj.config.proposalType;
+  const poolAmount = strategyObj.poolAmount as number;
   const spendingLimitPct =
-    (Number(strategyObj?.config?.maxRatio || 0) / CV_SCALE_PRECISION) * 100;
-
-  function calculateConvictionGrowthInDays(
-    decay: number,
-    blockTime: number,
-  ): number {
-    const halfLifeInSeconds =
-      blockTime / (Math.log(decay / Math.pow(10, 7)) / Math.log(1 / 2));
-
-    const convictionGrowth = halfLifeInSeconds / (24 * 60 * 60);
-
-    return convictionGrowth;
-  }
-
-  function calculateMinimumConviction(weight: number, spendingLimit: number) {
-    const weightNum = Number(weight) / CV_SCALE_PRECISION;
-
-    const spendingLimitFraction = spendingLimit / 100;
-    const maxRatioNum = spendingLimitFraction / MAX_RATIO_CONSTANT;
-
-    let minimumConviction = weightNum / maxRatioNum ** 2;
-
-    minimumConviction = minimumConviction * 100;
-
-    return minimumConviction;
-  }
-
-  const blockTime = chainDataMap[chain].blockTime;
-  const poolConfig = [
-    {
-      label: "Min conviction",
-      value: `${calculateMinimumConviction(strategyObj?.config.weight, spendingLimitPct * MAX_RATIO_CONSTANT)}%`,
-      info: "% of Pool's voting weight needed to pass the smallest funding proposal possible. Higher funding requests demand greater conviction to pass.",
-    },
-    {
-      label: "Conviction growth",
-      value: `${calculateConvictionGrowthInDays(strategyObj?.config.decay, blockTime)}`,
-      info: "It's the time for conviction to reach proposal support. This parameter is logarithmic, represented as a half life",
-    },
-    {
-      label: "Min Threshold",
-      value: `${formatTokenAmount(strategyObj?.config?.minThresholdPoints, tokenGarden?.decimals)} `,
-      info: `A fixed amount of ${tokenGarden?.symbol} that overrides Minimum Conviction when the Pool's activated governance is low.`,
-    },
-    {
-      label: "Spending limit",
-      // TODO: check number for some pools, they have more zeros or another config ?
-      value: `${spendingLimitPct * MAX_RATIO_CONSTANT}%`,
-      info: "Max percentage of the pool funds that can be spent in a single proposal",
-    },
-  ];
-
-  const filteredPoolConfig =
-    poolTypes[proposalType] === "signaling" ?
-      poolConfig.filter(
-        (config) => !["Spending limit", "Min Threshold"].includes(config.label),
-      )
-    : poolConfig;
+    (Number(strategyObj.config.maxRatio || 0) / CV_SCALE_PRECISION) * 100;
 
   const isEnabled = data.cvstrategies?.[0]?.isEnabled as boolean;
 
   return (
     <div className="page-layout">
-      {/* Header */}
-      <section className="section-layout flex flex-col gap-0 overflow-hidden">
-        <PoolHeader
-          strategy={strategyObj}
-          poolId={poolId}
-          ipfsResult={ipfsResult}
-          isEnabled={isEnabled}
-        />
-        <p>{ipfsResult?.description}</p>
-        <div className="mb-10 mt-8 flex items-start gap-24">
-          <div className="flex flex-col gap-2 max-w-fit">
-            <Statistic label="pool type">
-              <Badge type={proposalType} />
-            </Statistic>
-
-            {poolTypes[proposalType] === "funding" && (
-              <Statistic label="funding token">
-                <Badge
-                  isCapitalize
-                  label={tokenGarden.symbol}
-                  icon={<Square3Stack3DIcon />}
-                />
-              </Statistic>
-            )}
-
-            <Statistic label="voting weight system">
-              <div className="flex flex-col gap-3 sm:flex-row">
-                <Badge
-                  label="conviction voting"
-                  classNames="text-secondary-content"
-                  icon={<ChartBarIcon />}
-                />
-                <Badge label={pointSystems[pointSystem]} icon={<BoltIcon />} />
-              </div>
-            </Statistic>
-          </div>
-          <div className="flex flex-col gap-4">
-            {filteredPoolConfig.map((config) => (
-              <div key={config.label} className="flex items-center gap-4">
-                <Statistic label={config.label}>
-                  <InfoIcon content={config.info}>
-                    <h3 className="text-neutral-content">{config.value} </h3>
-                  </InfoIcon>
-                </Statistic>
-              </div>
-            ))}
-          </div>
-        </div>
-        {!isEnabled ?
-          <div className="banner">
-            <ClockIcon className="h-8 w-8 text-secondary-content" />
-            <h6>Waiting for council approval</h6>
-          </div>
-        : <Image
-            src={poolTypes[proposalType] === "funding" ? blueLand : grassLarge}
-            alt="pool image"
-            className="h-12 w-full rounded-lg object-cover"
-          />
-        }
-      </section>
-
+      <PoolHeader
+        token={tokenGarden}
+        strategy={strategyObj}
+        poolId={poolId}
+        ipfsResult={ipfsResult}
+        isEnabled={isEnabled}
+        pointSystem={pointSystem}
+        chainId={chain}
+        proposalType={proposalType}
+        spendingLimitPct={spendingLimitPct}
+      />
       {isEnabled && (
         <>
           {poolTypes[proposalType] !== "signaling" && (
