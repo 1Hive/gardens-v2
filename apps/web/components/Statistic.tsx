f77bec4324b6b90c1dd5e427fabd24a07de251be
Lucianosc <scaminaci.luciano@gmail.com>
2024-08-12 17:25:23 -0300

    add tooltip to Statistic

--- apps/web/components/Statistic.tsx
+++ apps/web/components/Statistic.tsx
@@ -6,6 +6,7 @@ type IdentifierProps = {
   count?: number | string;
   label: string;
   children?: React.ReactNode;
+  tooltip?: string;
 };
 
 export const Statistic = ({
@@ -13,17 +14,21 @@ export const Statistic = ({
   count,
   label,
   children,
+  tooltip,
 }: IdentifierProps) => {
-  const iconClassNames = "h-6 w-6";
-
   return (
     <div className="flex items-center gap-2 text-neutral-soft-content">
-      {icon && <div className={iconClassNames}>{icon}</div>}
-      {label && (
-        <p>
-          {capitalize(label)}: {count}
-        </p>
-      )}
+      <div
+        className={"tooltip flex cursor-default items-center max-w-sm"}
+        data-tip={tooltip}
+      >
+        {icon && <div className="mr-1">{icon}</div>}
+        {label && (
+          <p>
+            {capitalize(label)}: {count}
+          </p>
+        )}
+      </div>
       {children}
     </div>
   );
