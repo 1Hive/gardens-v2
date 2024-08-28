f0f1caa0f2b8ae109da5d187718f54d0f8c4cdbf
Corantin <corantin.noll@gmail.com>
2024-08-28 13:46:38 -0400

    Trycatch

--- apps/web/configs/isProd.ts
+++ apps/web/configs/isProd.ts
@@ -1,4 +1,9 @@
-const envFromStorage = localStorage?.getItem("env");
+let envFromStorage;
+try {
+  envFromStorage = localStorage?.getItem("env");
+} catch (error) {
+  // ignore when not browser side
+}
 export const isProd =
   envFromStorage ? envFromStorage : (
     process.env.NEXT_PUBLIC_ENV_GARDENS === "prod"
