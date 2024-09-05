38d4580efc759985e0951a53ea969bde4638c749
Lucianosc <scaminaci.luciano@gmail.com>
2024-09-05 16:16:42 -0300

    minor fix

--- apps/web/components/PoolCard.tsx
+++ apps/web/components/PoolCard.tsx
@@ -64,7 +64,7 @@ export function PoolCard({ pool, tokenGarden }: Props) {
           label="proposals"
         />
         {poolType && PoolTypes[poolType] === "funding" && (
-          <Statistic icon={<CurrencyDollarIcon />} label="funds available">
+          <Statistic icon={<CurrencyDollarIcon />} label="funds">
             <DisplayNumber
               number={[BigInt(poolAmount), tokenGarden.decimals]}
               compact={true}
