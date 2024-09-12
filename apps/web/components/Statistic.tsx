2a9af652743e3990711d39bea497b42ef454c0cd
Matias <mantitaaa89@gmail.com>
2024-09-12 15:56:27 -0300

    fix  tooltip hover in `Statistic`

--- apps/web/components/Statistic.tsx
+++ apps/web/components/Statistic.tsx
@@ -21,8 +21,11 @@ export const Statistic = ({
     <div
       className={`flex items-center gap-2 text-neutral-soft-content ${className}`}
     >
-      {icon && <div className="w-6 h-6">{icon}</div>}
-      <div className={"tooltip flex items-center max-w-sm"} data-tip={tooltip}>
+      <div
+        className={"tooltip flex items-center gap-2 max-w-sm"}
+        data-tip={tooltip}
+      >
+        {icon && <div className="w-6 h-6">{icon}</div>}
         {label && (
           <p className="first-letter:uppercase">
             {label}: {count}
