e6fbec9a25a0fa1fdf89e63fd327b6d0cd6676c8
Matias <mantitaaa89@gmail.com>
2024-09-16 15:10:15 -0300

    feat: add shadow to banner in landing page

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -226,7 +226,7 @@ const Banner = () => {
     <>
       {openBanner && (
         <div className="pointer-events-none fixed inset-x-0 bottom-0 sm:flex sm:justify-center sm:px-6 sm:pb-5 lg:px-8">
-          <div className="pointer-events-auto flex items-center justify-between gap-x-6 bg-primary-soft px-6 py-2.5 sm:rounded-xl sm:py-3 sm:pl-4 sm:pr-3.5">
+          <div className="pointer-events-auto flex items-center justify-between gap-x-6 bg-primary-soft px-6 py-2.5 sm:rounded-xl sm:py-3 sm:pl-4 sm:pr-3.5 shadow-xl">
             <p className="subtitle2">
               <a
                 href="https://juicebox.money/v2/p/697?np=1&tabid=nft_rewards"
