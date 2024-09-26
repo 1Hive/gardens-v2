1ef6e53b9d5a1152ca4d72b0c6675e4dc0acb687
Corantin <corantin.noll@gmail.com>
2024-09-26 13:21:15 -0400

    Update collaterals vault to fallback on available deposits

---
+++ pkg/subgraph/src/mappings/collateral-vault.ts
@@ -0,0 +1,70 @@
+import {
+  CollateralVault,
+  CollateralVaultDeposit
+} from "../../generated/schema";
+import {
+  CollateralVault as CollateralVaultContract,
+  CollateralDeposited,
+  CollateralWithdrawn,
+  CollateralWithdrawn1
+} from "../../generated/templates/CollateralVault/CollateralVault";
+
+export function handleCollateralDeposited(event: CollateralDeposited): void {
+  let entity = CollateralVault.load(event.address.toHexString());
+  if (entity == null) {
+    entity = new CollateralVault(event.address.toHexString());
+  }
+  const contract = CollateralVaultContract.bind(event.address);
+  entity.strategy = contract.owner().toHexString();
+
+  let deposit = new CollateralVaultDeposit(
+    event.address.toHexString() +
+      "-" +
+      event.params.proposalId +
+      "-" +
+      event.params.user
+  );
+
+  deposit.collateralVault = event.address.toHexString();
+  deposit.amount = event.params.amount;
+  deposit.depositor = event.params.user;
+  deposit.createdAt = event.block.timestamp;
+  deposit.proposalId = event.params.proposalId;
+  deposit.save();
+
+  entity.save();
+}
+
+export function handleCollateralWithdrawn(event: CollateralWithdrawn): void {
+  let deposit = new CollateralVaultDeposit(
+    event.address.toHexString() +
+      "-" +
+      event.params.proposalId +
+      "-" +
+      event.params.user
+  );
+
+  deposit.amount = deposit.amount.minus(event.params.amount);
+  deposit.withdrawnAt = event.block.timestamp;
+  deposit.withdrawnTo = event.params.user;
+
+  deposit.save();
+}
+
+export function handleCollateralWithdrawnFor(
+  event: CollateralWithdrawn1
+): void {
+  let deposit = new CollateralVaultDeposit(
+    event.address.toHexString() +
+      "-" +
+      event.params.proposalId +
+      "-" +
+      event.params.fromUser
+  );
+
+  deposit.amount = deposit.amount.minus(event.params.amount);
+  deposit.withdrawnAt = event.block.timestamp;
+  deposit.withdrawnTo = event.params.toUser;
+
+  deposit.save();
+}
