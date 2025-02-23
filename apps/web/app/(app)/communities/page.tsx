9898f458fabaea2fa3dfa402dffdb93859a3b6a6
Lucianosc <luchotandil90@gmail.com>
2025-02-23 15:55:18 +0100

    feat: communities page

---
+++ apps/web/app/(app)/communities/page.tsx
@@ -0,0 +1,126 @@
+"use client";
+
+import React from "react";
+import { PlusIcon } from "@heroicons/react/24/outline";
+import Image from "next/image";
+import Link from "next/link";
+import {
+  getCommunitiesDocument,
+  getCommunitiesQuery,
+} from "#/subgraph/.graphclient";
+import { clouds1, clouds2, grassLarge, tree2, tree3 } from "@/assets";
+import { Button, Communities } from "@/components";
+import { ChainIcon, getConfigByChain } from "@/configs/chains";
+import { useDisableButtons } from "@/hooks/useDisableButtons";
+import { useSubgraphQueryMultiChain } from "@/hooks/useSubgraphQueryMultiChain";
+
+// Components
+const Header = () => (
+  <header className="flex flex-col items-center gap-8">
+    <div className="flex items-center text-center">
+      <div className="relative flex-1">
+        <Image src={clouds1} alt="clouds" width={205} height={205} />
+      </div>
+      <div className="mx-10 flex flex-col items-center gap-5">
+        <div className="flex flex-col items-center">
+          <h1 className="max-w-xl text-center text-neutral-content">
+            Welcome to Gardens
+          </h1>
+          <p className="text-xl text-primary-content text-center">
+            A place where communities grow through collective decision-making
+          </p>
+        </div>
+      </div>
+      <div className="relative flex-1">
+        <Image src={clouds2} alt="clouds" width={205} height={205} />
+      </div>
+    </div>
+  </header>
+);
+
+const Footer = () => {
+  const { tooltipMessage, isConnected, missmatchUrl } = useDisableButtons();
+
+  return (
+    <section className="section-layout">
+      <div className="flex flex-col gap-10 overflow-x-hidden">
+        <header>
+          <h4 className="text-secondary-content">Create your own community</h4>
+        </header>
+        <div className="relative flex h-[219px] justify-center">
+          <Link href="/communities/create-community" className="mt-6 z-10">
+            <Button
+              btnStyle="filled"
+              disabled={!isConnected || missmatchUrl}
+              tooltip={tooltipMessage}
+              icon={<PlusIcon height={24} width={24} />}
+            >
+              Create a community
+            </Button>
+          </Link>
+          <Image
+            src={tree2}
+            alt="tree"
+            className="absolute bottom-0 -left-10 h-52"
+          />
+          <Image
+            src={tree3}
+            alt="tree"
+            className="absolute bottom-0 -right-10 h-60"
+          />
+          <Image
+            src={grassLarge}
+            alt="grass"
+            className="absolute bottom-0 min-w-[1080px]"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+// Main component
+export default function GardensPage() {
+  const { data: communitiesSections, fetching } =
+    useSubgraphQueryMultiChain<getCommunitiesQuery>({
+      query: getCommunitiesDocument,
+
+      changeScope: [
+        // {
+        //   topic: "garden",
+        // },
+        {
+          topic: "community",
+        },
+      ],
+    });
+
+  return (
+    <div className="page-layout">
+      <Header />
+      {communitiesSections?.map(({ registryCommunities }) => (
+        <Communities
+          communities={registryCommunities}
+          header={
+            <div className="align-start flex flex-col justify-start">
+              <p className="text-neutral-content text-sm">Network:</p>
+              <div className="flex gap-2.5 items-center mt-1">
+                <h5 className="text-neutral-content">
+                  {getConfigByChain(registryCommunities?.[0]?.chainId)?.name}
+                </h5>
+                <div className="flex content-center justify-center">
+                  <ChainIcon
+                    chain={registryCommunities?.[0]?.chainId}
+                    height={24}
+                  />
+                </div>
+              </div>
+            </div>
+          }
+          key={`section-${registryCommunities?.[0]?.chainId}`}
+        />
+      ))}
+      <Footer />
+    </div>
+  );
+}
