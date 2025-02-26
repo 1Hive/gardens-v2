2fd7321a873e2739f159ba8bf0bae8eb5b2de77e
Lucianosc <luchotandil90@gmail.com>
2025-02-26 15:59:16 +0100

    add(styles): changes in CommunityCard

--- apps/web/components/CommunityCard.tsx
+++ apps/web/components/CommunityCard.tsx
@@ -3,19 +3,24 @@
 import React from "react";
 import { RectangleGroupIcon, UserGroupIcon } from "@heroicons/react/24/outline";
 import Image from "next/image";
-import { CVStrategy, Maybe, MemberCommunity } from "#/subgraph/.graphclient";
+import {
+  CVStrategy,
+  Maybe,
+  MemberCommunity,
+  TokenGarden,
+} from "#/subgraph/.graphclient";
 import { Card } from "./Card";
 import { Statistic } from "./Statistic";
 import TooltipIfOverflow from "./TooltipIfOverflow";
 import { commImg } from "@/assets";
+import { ChainIcon } from "@/configs/chains";
 import { QUERY_PARAMS } from "@/constants/query-params";
 import { useCollectQueryParams } from "@/contexts/collectQueryParams.context";
 
 type CommunityCardProps = {
   id: string;
   communityName?: Maybe<string> | undefined;
-  chainId: number;
-  registerToken?: Maybe<string> | undefined;
+  garden: Pick<TokenGarden, "address" | "chainId" | "symbol">;
   members?: Maybe<Pick<MemberCommunity, "id" | "memberAddress">[]> | undefined;
   strategies?: Maybe<Pick<CVStrategy, "id">[]> | undefined;
 };
@@ -23,11 +28,12 @@ type CommunityCardProps = {
 export function CommunityCard({
   id,
   communityName,
-  chainId,
-  registerToken,
+  garden,
   members,
   strategies,
 }: CommunityCardProps) {
+  const { address: tokenAddr, chainId, symbol: tokenSymbol } = garden;
+
   const membersCount = members?.length ?? 0;
   const poolsCount = strategies?.length ?? 0;
 
@@ -39,20 +45,34 @@ export function CommunityCard({
   return (
     <Card
       key={id}
-      href={`/gardens/${chainId}/${registerToken}/${id}`}
+      href={`/gardens/${chainId}/${tokenAddr}/${id}`}
       className={`w-[275px] sm:min-w-[313px] ${isNewCommunity ? "shadow-2xl" : ""}`}
     >
-      <Image
-        src={commImg}
-        alt={`${name} community`}
-        className="mb-2 h-[100px]"
-        height={100}
-        width={100}
-      />
+      <div className="flex justify-between text-neutral-content text-sm">
+        <Image
+          src={commImg}
+          alt={`${name} community`}
+          className="mb-2 h-[100px]"
+          height={100}
+          width={100}
+        />
+        <div className="flex flex-col gap-1">
+          <div className="flex gap-2 items-center">
+            <ChainIcon chain={chainId} height={24} />
+          </div>
+        </div>
+      </div>
+
       <div className="flex flex-col gap-2">
         <h3 className="flex items-start w-fit max-w-full">
           <TooltipIfOverflow>{communityName ?? ""}</TooltipIfOverflow>
         </h3>
+        <div className="flex gap-2 items-center">
+          <p className="text-base font-normal">
+            Governance Token:
+            <span className="text-base font-normal"> {tokenSymbol}</span>
+          </p>
+        </div>
         <Statistic
           label="members"
           count={membersCount}
