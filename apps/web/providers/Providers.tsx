2cc14060af40a2dd23d937f72e239d3cea227b3b
Lucianosc <luchotandil90@gmail.com>
2025-03-01 17:18:35 +0100

    refactor(Providers): wagmi chains support

--- apps/web/providers/Providers.tsx
+++ apps/web/providers/Providers.tsx
@@ -26,9 +26,57 @@ import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
 import ThemeProvider from "./ThemeProvider";
 import { UrqlProvider } from "./UrqlProvider";
+import { chainConfigMap, CHAINS } from "@/configs/chains";
+import { isProd } from "@/configs/isProd";
 import { QueryParamsProvider } from "@/contexts/collectQueryParams.context";
 import { PubSubProvider } from "@/contexts/pubsub.context";
 import { useChainFromPath } from "@/hooks/useChainFromPath";
+import { ChainId } from "@/types";
+
+const createCustomConfig = (chain: Chain | undefined) => {
+  let usedChains: Chain[] = [];
+  if (chain) {
+    usedChains = [chain, mainnet];
+  } else {
+    const usedChainIds: ChainId[] = Object.entries(chainConfigMap)
+      .filter(([_, chainConfig]) =>
+        isProd ? !chainConfig.isTestnet : !!chainConfig.isTestnet,
+      )
+      .map(([chainId]) => chainId);
+    usedChains = [
+      ...CHAINS.filter((elem) => usedChainIds.includes(elem.id.toString())),
+      mainnet,
+    ];
+  }
+
+  const { publicClient, chains } = configureChains(usedChains, [
+    publicProvider(),
+    alchemyProvider({
+      apiKey: process.env.NEXT_PUBLIC_ALCHEMY_KEY ?? "",
+    }),
+  ]);
+  const connectors = connectorsForWallets([
+    {
+      groupName: "Recommended",
+      wallets: [
+        injectedWallet({ chains }),
+        rabbyWallet({ chains }),
+        frameWallet({ chains }),
+        coinbaseWallet({ appName: "Gardens V2", chains }),
+        walletConnectWallet({
+          chains,
+          projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_ID ?? "",
+        }),
+      ],
+    },
+  ]);
+
+  return createConfig({
+    autoConnect: true,
+    connectors,
+    publicClient,
+  });
+};
 
 type Props = {
   children: React.ReactNode;
@@ -36,74 +84,46 @@ type Props = {
 
 const Providers = ({ children }: Props) => {
   const [mounted, setMounted] = useState(false);
-  useEffect(() => setMounted(true), []);
-  const [wagmiConfig, setWagmiConfig] =
-    useState<ReturnType<typeof createCustomConfig>>();
-  const chain = useChainFromPath() as Chain;
-  const createCustomConfig = () => {
-    const { publicClient, chains } = configureChains(
-      chain?.rpcUrls ? [chain, mainnet] : [mainnet],
-      [
-        publicProvider(),
-        alchemyProvider({
-          apiKey: process.env.NEXT_PUBLIC_ALCHEMY_KEY ?? "",
-        }),
-      ],
-    );
-    const connectors = connectorsForWallets([
-      {
-        groupName: "Recommended",
-        wallets: [
-          injectedWallet({ chains }),
-          rabbyWallet({ chains }),
-          frameWallet({ chains }),
-          coinbaseWallet({ appName: "Gardens V2", chains }),
-          walletConnectWallet({
-            chains,
-            projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_ID ?? "",
-          }),
-        ],
-      },
-    ]);
+  const chain = useChainFromPath() as Chain | undefined;
 
-    return createConfig({
-      autoConnect: true,
-      connectors,
-      publicClient,
-    });
-  };
+  const [wagmiConfig, setWagmiConfig] = useState(() =>
+    createCustomConfig(chain),
+  );
 
   useEffect(() => {
-    setWagmiConfig(createCustomConfig());
+    setMounted(true);
+  }, []);
+
+  useEffect(() => {
+    setWagmiConfig(createCustomConfig(chain));
   }, [chain]);
 
+  if (!mounted || !wagmiConfig) {
+    return null;
+  }
+
   return (
-    // if mounted UrlqProvider will be rendered
-    // if not, null will be rendered
-    mounted &&
-    wagmiConfig && (
-      <UrqlProvider>
-        <WagmiConfig config={wagmiConfig}>
-          <AddrethConfig>
-            <RainbowKitProvider
-              modalSize="compact"
-              chains={wagmiConfig.chains ?? []}
-              theme={lightTheme({
-                accentColor: "var(--color-primary)",
-                accentColorForeground: "var(--color-black)",
-                borderRadius: "large",
-              })}
-            >
-              <ThemeProvider>
-                <QueryParamsProvider>
-                  <PubSubProvider>{mounted && children}</PubSubProvider>
-                </QueryParamsProvider>
-              </ThemeProvider>
-            </RainbowKitProvider>
-          </AddrethConfig>
-        </WagmiConfig>
-      </UrqlProvider>
-    )
+    <UrqlProvider>
+      <WagmiConfig config={wagmiConfig}>
+        <AddrethConfig>
+          <RainbowKitProvider
+            modalSize="compact"
+            chains={wagmiConfig.chains ?? []}
+            theme={lightTheme({
+              accentColor: "var(--color-primary)",
+              accentColorForeground: "var(--color-black)",
+              borderRadius: "large",
+            })}
+          >
+            <ThemeProvider>
+              <QueryParamsProvider>
+                <PubSubProvider>{children}</PubSubProvider>
+              </QueryParamsProvider>
+            </ThemeProvider>
+          </RainbowKitProvider>
+        </AddrethConfig>
+      </WagmiConfig>
+    </UrqlProvider>
   );
 };
 
