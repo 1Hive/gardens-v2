46c6f04afa04db665be1f79d54b96fa0bb13aebd
Matias <mantitaaa89@gmail.com>
2025-03-27 21:51:54 -0300

    feat: Update `Footer` props type

--- apps/web/components/LandingPage/Footer.tsx
+++ apps/web/components/LandingPage/Footer.tsx
@@ -1,6 +1,7 @@
 import { motion } from "motion/react";
 import Image from "next/image";
 import { newLogo } from "@/assets";
+import { JSX, SVGProps } from "react";
 
 const navigation = {
   solutions: [
@@ -30,7 +31,7 @@ const navigation = {
     {
       name: "X",
       href: "#",
-      icon: (props) => (
+      icon: (props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
           <path d="M13.6823 10.6218L20.2391 3H18.6854L12.9921 9.61788L8.44486 3H3.2002L10.0765 13.0074L3.2002 21H4.75404L10.7663 14.0113L15.5685 21H20.8131L13.6819 10.6218H13.6823ZM11.5541 13.0956L10.8574 12.0991L5.31391 4.16971H7.70053L12.1742 10.5689L12.8709 11.5655L18.6861 19.8835H16.2995L11.5541 13.096V13.0956Z" />
         </svg>
@@ -39,7 +40,7 @@ const navigation = {
     {
       name: "GitHub",
       href: "#",
-      icon: (props) => (
+      icon: (props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
           <path
             fillRule="evenodd"
@@ -52,7 +53,7 @@ const navigation = {
     {
       name: "Wrapcast",
       href: "#",
-      icon: (props) => (
+      icon: (props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
           <path
             fillRule="evenodd"
