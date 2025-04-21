86dc9e65062e3fe613f315605b3284a0f815cb5f
Matias <mantitaaa89@gmail.com>
2025-04-21 11:46:56 -0300

    added pool #131, #132 to matching pools array

--- apps/web/utils/matchingPools.ts
+++ apps/web/utils/matchingPools.ts
@@ -1,2 +1,2 @@
 // includes every matching pool by Id to be displayed in the PoolCard and Pool id page
-export const elegibleGG23pools = [559, 563, 849];
+export const elegibleGG23pools = [559, 563, 849, 131, 132];
