2e5a067416175969b742ded1a5eb61206d22f125
Kevin Mansour <75225303+kafann@users.noreply.github.com>
2025-08-06 21:10:22 -0400

    Fix s in ui conviction growth + initial api PR

---
+++ apps/web/utils/goodollar.ts
@@ -0,0 +1,27 @@
+import { IdentitySDK } from "@goodsdks/citizen-sdk";
+import { createPublicClient, http, createWalletClient, custom } from "viem";
+import { Address as AddressType } from "viem";
+import { celo } from "viem/chains";
+export async function fetchGooddollarWhitelisted(
+  account: string,
+): Promise<boolean> {
+  const celoPublicClient = createPublicClient({
+    chain: celo,
+    transport: http("https://forno.celo.org"),
+  });
+
+  const celoWalletClient = createWalletClient({
+    chain: celo,
+    transport: custom(celoPublicClient.transport),
+  });
+  const celoIdentitySDK = new IdentitySDK(
+    celoPublicClient,
+    celoWalletClient,
+    "development",
+  );
+  console.log("CALLED");
+  const { isWhitelisted, root } = await celoIdentitySDK!.getWhitelistedRoot(
+    account as AddressType,
+  );
+  return isWhitelisted;
+}
