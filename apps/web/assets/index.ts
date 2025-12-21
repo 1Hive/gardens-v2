2205b8eb112be7ab544d008d4057811d4e1caebe
Mati.M.M <mantitaaa89@gmail.com>
2025-12-21 18:26:38 -0300

    refactor, feat: move campaigns init data to config and extend Superfluid campaigns support - update fetch logic to map multiple Superfluid campaigns - add GD campaign card with metadata and stats - include campaign banner and logo

--- apps/web/assets/index.ts
+++ apps/web/assets/index.ts
@@ -40,5 +40,7 @@ export { default as GreenWave } from "./GreenWave.png";
 export { default as OneHiveLogo } from "./OneHiveLogo.png";
 export { default as SuperfluidStream } from "./ReceivingStream-green.png";
 export { default as SuperLogo } from "./superfluid-logo-dark.svg";
+export { default as GoodDollarLogo } from "./gd-logo.svg";
 export { default as SuperBanner } from "./superfluid-banner.png";
 export { default as PlantBanner } from "./plante-banner.png";
+export { default as GoodDollarBanner } from "./gd-banner.png";
