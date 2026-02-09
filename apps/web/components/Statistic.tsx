685dc8d810de3c05214df3256c2cfbaf7e322d6a
Mati.M.M <mantitaaa89@gmail.com>
2026-02-09 16:09:15 -0300

    new, more nit PoolCard + small tweak in Stattistic component

--- apps/web/components/Statistic.tsx
+++ apps/web/components/Statistic.tsx
@@ -19,18 +19,18 @@ export const Statistic = ({
 }: IdentifierProps) => {
   return (
     <div
-      className={`flex items-center gap-1 text-neutral-soft-content ${className}`}
+      className={`flex items-center text-neutral-soft-content ${icon ? "gap-1" : "gap-0"} ${className} `}
     >
       <div
         className={"tooltip flex items-center gap-2 max-w-sm"}
         data-tip={tooltip}
       >
         {icon != null && (
-          <div className="w-5 h-5 sm:w-6 sm:h-6 cursor-pointer">{icon}</div>
+          <div className="w-5 h-5 sm:w-5 sm:h-5 cursor-pointer">{icon}</div>
         )}
       </div>
       {label != null && (
-        <p className="first-letter:uppercase text-sm sm:text-md text-neutral-content ">
+        <p className="first-letter:uppercase text-sm sm:text-md text-neutral-content">
           {label}: {count}
         </p>
       )}
