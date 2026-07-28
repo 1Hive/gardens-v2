c8675b1ae9d8d30e3dcbfd941d3ff69a689ebc5d
Corantin <corantin.noll@gmail.com>
2026-07-28 11:26:01 -0400

    fix(web): handle optional covenant hash

--- apps/web/hooks/useCovenantAgreementSignature.tsx
+++ apps/web/hooks/useCovenantAgreementSignature.tsx
@@ -27,7 +27,7 @@ export function useCovenantAgreementSignature(
   }: {
     chainId: number | undefined;
     communityAddress: Address;
-    covenant: string;
+    covenant: string | null | undefined;
   },
 ): {
   covenantAgreementTxProps: TransactionProps;
@@ -84,11 +84,12 @@ export function useCovenantAgreementSignature(
       }
 
       const storageKey =
-        chainId == null
+        chainId == null || covenant == null || !address
           ? undefined
           : getCovenantSignatureKey({
               chainId,
               communityAddress,
+              accountAddress: address,
               covenant,
             });
 
@@ -103,9 +104,7 @@ export function useCovenantAgreementSignature(
           message: getTxMessage("success"),
           status: "success",
         });
-        triggerNextTx({
-          covenantSignature: cachedSignature.signature ?? "",
-        });
+        triggerNextTx({ covenantSignature: cachedSignature.signature });
         return;
       }
 
